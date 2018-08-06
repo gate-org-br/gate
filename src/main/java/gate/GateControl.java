e40b5f848d6dd139824aff31642d5400a92d23a0
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2018-08-06 19:15:38 -0300

    Fallback to gate authentication when user does not exists on authenticator server

--- src/main/java/gate/GateControl.java
+++ src/main/java/gate/GateControl.java
@@ -1,14 +1,14 @@
 package gate;
 
-import gate.error.InvalidPasswordException;
-import gate.error.InvalidUsernameException;
-import gate.error.InvalidServiceException;
-import gate.error.AuthenticatorException;
 import gate.authenticator.LDAPAuthenticator;
 import gate.entity.Org;
 import gate.entity.User;
+import gate.error.AuthenticatorException;
 import gate.error.ConstraintViolationException;
 import gate.error.DefaultPasswordException;
+import gate.error.InvalidPasswordException;
+import gate.error.InvalidServiceException;
+import gate.error.InvalidUsernameException;
 import gate.type.MD5;
 import javax.enterprise.context.Dependent;
 
@@ -19,21 +19,21 @@ class GateControl extends gate.base.Control
 	private static final LDAPAuthenticator AUTHENTICATOR = new LDAPAuthenticator();
 
 	public User select(Org org,
-			   String username,
-			   String password) throws
-		InvalidServiceException,
-		InvalidUsernameException,
-		InvalidPasswordException,
-		DefaultPasswordException,
-		AuthenticatorException
+			String username,
+			String password) throws
+			InvalidServiceException,
+			InvalidUsernameException,
+			InvalidPasswordException,
+			DefaultPasswordException,
+			AuthenticatorException
 
 	{
 
 		try (GateDao dao = new GateDao())
 		{
 			User user = dao.getUser(username)
-				.orElseThrow(()
-					-> new InvalidUsernameException());
+					.orElseThrow(()
+							-> new InvalidUsernameException());
 
 			if (user.isDisabled())
 				throw new InvalidUsernameException();
@@ -45,8 +45,17 @@ class GateControl extends gate.base.Control
 				throw new InvalidUsernameException();
 
 			if (!org.getAuthenticators().isEmpty())
-				AUTHENTICATOR.authenticate(org.getAuthenticators(), username, password);
-			else if (!user.getPasswd().equals(MD5.digest(password).toString()))
+			{
+				try
+				{
+					AUTHENTICATOR.authenticate(org.getAuthenticators(),
+							username, password);
+				} catch (InvalidUsernameException ex)
+				{
+					if (!user.getPasswd().equals(MD5.digest(password).toString()))
+						throw new InvalidPasswordException();
+				}
+			} else if (!user.getPasswd().equals(MD5.digest(password).toString()))
 				throw new InvalidPasswordException();
 
 			if (username.equals(password))
