af55c13af829a482809ae237e7c7b8c17bed2817
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2018-08-08 07:12:56 -0300

    Implement email authentication

--- src/main/java/gate/Gate.java
+++ src/main/java/gate/Gate.java
@@ -104,12 +104,7 @@ public class Gate extends HttpServlet
 
 				if (!Toolkit.isEmpty(username)
 						&& !Toolkit.isEmpty(password))
-				{
-					int indexOf = username.indexOf("@");
-					if (indexOf != -1)
-						username = username.substring(0, indexOf);
 					session.setUser(user = control.select(org, username, password));
-				}
 
 				Class<Screen> clazz = Screen.getScreen(MODULE, SCREEN).orElseThrow(InvalidRequestException::new);
 				Method method = Screen.getAction(clazz, ACTION).orElseThrow(InvalidRequestException::new);
