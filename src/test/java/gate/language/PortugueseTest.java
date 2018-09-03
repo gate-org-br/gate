375688c08a58535b99efaac0ade8e12cce079f12
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2018-09-03 09:26:16 -0300

    Implement methods to compare and find substrings ignore non ASCII chars

--- src/test/java/gate/language/PortugueseTest.java
+++ src/test/java/gate/language/PortugueseTest.java
@@ -7,21 +7,13 @@ public class PortugueseTest
 {
 
 	@Test
-	public void testCapitalizeWord()
-	{
-		Assert.assertEquals("Fulano", Language.PORTUGUESE.capitalizeWord("fulano"));
-		Assert.assertEquals("Fulano", Language.PORTUGUESE.capitalizeWord("FULANO"));
-		Assert.assertEquals("Fulano", Language.PORTUGUESE.capitalizeWord("FuLaNo"));
-	}
-
-	@Test
 	public void testCapitalizeSentence()
 	{
 		Assert.assertEquals("Fulano da Silva dos Santos Souza",
-				Language.PORTUGUESE.capitalizeName("fulano da silva dos santos souza"));
+				Language.PORTUGUESE.capitalize("fulano da silva dos santos souza"));
 		Assert.assertEquals("Fulano da Silva dos Santos Souza",
-				Language.PORTUGUESE.capitalizeName("FULANO DA SILVA DOS SANTOS SOUZA"));
+				Language.PORTUGUESE.capitalize("FULANO DA SILVA DOS SANTOS SOUZA"));
 		Assert.assertEquals("Fulano da Silva dos Santos Souza",
-				Language.PORTUGUESE.capitalizeName("fulano DA silva DOS santos SOUZA"));
+				Language.PORTUGUESE.capitalize("fulano DA silva DOS santos SOUZA"));
 	}
 }
