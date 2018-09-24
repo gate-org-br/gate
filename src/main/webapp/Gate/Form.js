1091a6cb58e47d1859e86264a41745f854ddfafd
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2018-09-24 19:36:50 -0300

    Improve form key press behavior:   Enter key now functions like a tab key   Control enter key now submits the form   Space on buttons and links now functions like a click

--- src/main/webapp/Gate/Form.js
+++ src/main/webapp/Gate/Form.js
@@ -30,103 +30,82 @@ window.addEventListener("load", function ()
 					.show();
 			}
 		});
+
 		form.addEventListener("keydown", function (event)
 		{
 			event = event ? event : window.event;
+
 			if (event.keyCode === 13)
 			{
-				var input = document.activeElement;
-				switch (input.tagName.toLowerCase())
+				if (this.hasAttribute("action") && event.ctrlKey)
 				{
-					case "input":
-						if (input.hasAttribute("type"))
-							switch (input.getAttribute("type").toLowerCase())
-							{
-								case "color":
-								case "date":
-								case "datetime-local":
-								case "email":
-								case "month":
-								case "number":
-								case "file":
-								case "range":
-								case "search":
-								case "tel":
-								case "time":
-								case "url":
-								case "week":
-								case "text":
-								case "password":
-									input = next(input);
-									if (input)
-										input.focus();
-									break;
-								case "submit":
-									input.click();
-									break;
-							}
-						event.preventDefault();
-						event.stopImmediatePropagation();
-						break;
-					case "select":
-						input = next(input);
+					var button = document.createElement("button");
+					button.style.display = "none";
+					this.appendChild(button);
+					button.click();
+					this.removeChild(button);
+					event.preventDefault();
+					event.stopImmediatePropagation();
+				} else
+				{
+					var input = document.activeElement;
+					if (input.hasAttribute("tabindex")
+						&& input.tagName.toLowerCase() !== "textarea")
+					{
+						input = next(input, event.shiftKey);
 						if (input)
 							input.focus();
 						event.preventDefault();
 						event.stopImmediatePropagation();
-						break;
-					case "button":
-						input.click();
-						event.preventDefault();
-						event.stopImmediatePropagation();
-						break;
-					case "textarea":
-						if (event.ctrlKey)
-						{
-							input = next(input);
-							if (input)
-								input.focus();
-							event.preventDefault();
-							event.stopImmediatePropagation();
-						}
-						break;
+					}
 				}
 			}
 
-			function next(input)
+			function next(input, shift)
 			{
-				var inputs = Array.from(input.form.elements)
-					.filter(function (e)
-					{
-						return e.offsetParent;
-					})
-					.map(function (e, index)
-					{
-						return {obj: e, idx: index};
-					});
+				var form = input.parentNode;
+				while (form.tagName.toLowerCase() !== "form")
+					form = form.parentNode;
+
+				var inputs = Array.from(form.getElementsByTagName("*")).filter(function (e)
+				{
+					return e.tagName
+						&& e.offsetParent
+						&& e.hasAttribute("tabindex")
+						&& (e.tagName.toLowerCase() === "input"
+							|| e.tagName.toLowerCase() === "select"
+							|| e.tagName.toLowerCase() === "textarea"
+							|| e.tagName.toLowerCase() === "a"
+							|| e.tagName.toLowerCase() === "button");
+				}).map(function (e, index)
+				{
+					return {obj: e, idx: index};
+				});
+
 				inputs.sort(function (e1, e2)
 				{
 					var tabindex1 = e1.obj.getAttribute("tabindex");
 					var tabindex2 = e2.obj.getAttribute("tabindex");
 					if (tabindex1 === tabindex2)
 						return e1.idx - e2.idx;
-					if (tabindex1 && !tabindex2)
-						return -1;
-					if (!tabindex1 && tabindex2)
-						return 1;
 					return Number(tabindex1) - Number(tabindex2);
 				});
+
+				if (shift)
+					inputs = inputs.reverse();
+
 				inputs = inputs.map(function (e)
 				{
 					return e.obj;
 				});
 				for (var i = 0; i < inputs.length; i++)
-					if (inputs[i] === input && inputs.length >= i + 1)
+					if (inputs[i] === input && inputs.length > i + 1)
 						return inputs[i + 1];
+
+				return inputs.length > 0 ? inputs[0] : null;
 			}
 		});
-	}
-	);
+	});
 });
 
 
