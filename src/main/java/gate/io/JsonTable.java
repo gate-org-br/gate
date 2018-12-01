8849a4fc2c28b5594c248510613a48de45596b0f
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2018-12-01 08:44:46 -0300

    Make so that JsonTable process all data in memory before disk access

--- src/main/java/gate/io/JsonTable.java
+++ src/main/java/gate/io/JsonTable.java
@@ -2,26 +2,13 @@ package gate.io;
 
 import gate.annotation.SecurityKey;
 import gate.converter.Converter;
-import gate.error.AppError;
-import gate.util.IO;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterInputStream;
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 
 class JsonTable<T> extends AbstractTable<T>
 {
@@ -34,42 +21,27 @@ class JsonTable<T> extends AbstractTable<T>
 	@Override
 	protected void persist()
 	{
-		if (!values.isEmpty())
+		try
 		{
-			if (file.getParentFile() != null)
-				file.getParentFile().mkdirs();
-			try (FileOutputStream fileOutputStream = new FileOutputStream(file);
-					DeflaterOutputStream deflaterOutputStream = new DeflaterOutputStream(fileOutputStream))
+			if (!values.isEmpty())
 			{
-				String json = Converter.toJson(values);
-				if (type.isAnnotationPresent(SecurityKey.class))
-				{
-					Cipher cipher = Cipher.getInstance(ALGORITHM);
-					String stringKey = type.getAnnotation(SecurityKey.class).value();
-					byte[] key = DatatypeConverter.parseBase64Binary(stringKey);
-					SecretKeySpec secretKeySpec = new SecretKeySpec(key, ALGORITHM);
-					cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+				if (file.getParentFile() != null)
+					file.getParentFile().mkdirs();
 
-					try (CipherOutputStream cipherOutputStream = new CipherOutputStream(deflaterOutputStream, cipher);
-							BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(cipherOutputStream)))
-					{
-						writer.write(json);
-						writer.flush();
-					}
-				} else
-				{
-					try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(deflaterOutputStream)))
-					{
-						writer.write(json);
-						writer.flush();
-					}
-				}
-			} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IOException ex)
-			{
-				throw new AppError(ex);
-			}
-		} else
-			file.delete();
+				byte[] bytes = Converter.toJson(values).getBytes();
+
+				if (type.isAnnotationPresent(SecurityKey.class))
+					bytes = Encryptor.of(ALGORITHM, type.getAnnotation(SecurityKey.class).value()).encrypt(bytes);
+
+				bytes = Compactor.compact(bytes);
+
+				Files.write(file.toPath(), bytes, StandardOpenOption.CREATE);
+			} else
+				file.delete();
+		} catch (IOException ex)
+		{
+			throw new UncheckedIOException(ex);
+		}
 	}
 
 	static <T> JsonTable<T> create(Class<T> type, File file)
@@ -77,32 +49,19 @@ class JsonTable<T> extends AbstractTable<T>
 		if (!file.exists() || file.length() == 0)
 			return new JsonTable<>(type, file, new HashSet<>());
 
-		try (FileInputStream fileInputStream = new FileInputStream(file);
-				InflaterInputStream inflaterInputStream = new InflaterInputStream(fileInputStream))
+		try
 		{
-			if (type.isAnnotationPresent(SecurityKey.class))
-			{
-				Cipher cipher = Cipher.getInstance(ALGORITHM);
-				String stringKey = type.getAnnotation(SecurityKey.class).value();
-				byte[] key = DatatypeConverter.parseBase64Binary(stringKey);
-				SecretKeySpec secretKeySpec = new SecretKeySpec(key, ALGORITHM);
-				cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+			byte[] bytes = Files.readAllBytes(file.toPath());
+			bytes = Compactor.extract(bytes);
 
-				try (CipherInputStream cipherInputStream = new CipherInputStream(inflaterInputStream, cipher))
-				{
-					String json = IO.readString(cipherInputStream);
-					Set<T> values = Converter.fromJson(Set.class, type, json);
-					return new JsonTable<>(type, file, values != null ? values : new HashSet<>());
-				}
-			} else
-			{
-				String json = IO.readString(inflaterInputStream);
-				Set<T> values = Converter.fromJson(Set.class, type, json);
-				return new JsonTable<>(type, file, values != null ? values : new HashSet<>());
-			}
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IOException ex)
+			if (type.isAnnotationPresent(SecurityKey.class))
+				bytes = Encryptor.of(ALGORITHM, type.getAnnotation(SecurityKey.class).value()).decrypt(bytes);
+
+			Set<T> values = Converter.fromJson(Set.class, type, new String(bytes));
+			return new JsonTable<>(type, file, values != null ? values : new HashSet<>());
+		} catch (IOException ex)
 		{
-			throw new AppError(ex);
+			throw new UncheckedIOException(ex);
 		}
 	}
 }
