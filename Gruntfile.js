0f444ee2d283332445076121c7890a189f553e5e
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2018-12-05 11:16:54 -0300

    Change yuicompressor for grunt

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		watch: {
+			views: {
+				files: ['src/main/webapp/components/*.js'],
+				tasks: ['concat']
+			}
+		},
+		concat:
+			{
+				dist:
+					{
+						src: ["src/main/webapp/components/dialog-polyfill.js",
+							"src/main/webapp/components/Gate/Gate.js",
+							"src/main/webapp/components/Duration.js",
+							"src/main/webapp/components/Fetch.js",
+							"src/main/webapp/components/Keys.js",
+							"src/main/webapp/components/URL.js",
+							"src/main/webapp/components/Calendar.js",
+							"src/main/webapp/components/Link.js",
+							"src/main/webapp/components/Button.js",
+							"src/main/webapp/components/ActionHandler.js",
+							"src/main/webapp/components/ChangeHandler.js",
+							"src/main/webapp/components/Mask.js",
+							"src/main/webapp/components/Modal.js",
+							"src/main/webapp/components/Block.js",
+							"src/main/webapp/components/DateDialog.js",
+							"src/main/webapp/components/TimeDialog.js",
+							"src/main/webapp/components/DateTimeDialog.js",
+							"src/main/webapp/components/DateIntervalDialog.js",
+							"src/main/webapp/components/TimeIntervalDialog.js",
+							"src/main/webapp/components/DateTimeIntervalDialog.js",
+							"src/main/webapp/components/Chart.js",
+							"src/main/webapp/components/ChartDialog.js",
+							"src/main/webapp/components/PageControl.js",
+							"src/main/webapp/components/LinkControl.js",
+							"src/main/webapp/components/Dialog.js",
+							"src/main/webapp/components/Popup.js",
+							"src/main/webapp/components/TreeView.js",
+							"src/main/webapp/components/DateFormat.js",
+							"src/main/webapp/components/Desktop.js",
+							"src/main/webapp/components/Switch.js",
+							"src/main/webapp/components/Clock.js",
+							"src/main/webapp/components/RichTextarea.js",
+							"src/main/webapp/components/FullScreen.js",
+							"src/main/webapp/components/Form.js",
+							"src/main/webapp/components/Progress.js",
+							"src/main/webapp/components/Focus.js",
+							"src/main/webapp/components/Sortable.js",
+							"src/main/webapp/components/Message.js",
+							"src/main/webapp/components/Return.js",
+							"src/main/webapp/components/DeskMenu/DeskMenu.js",
+							"src/main/webapp/components/DeskPane/DeskPane.js"],
+						dest: 'src/main/webapp/Gate.js'
+					}
+			}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.registerTask('default', ['watch']);
+};
--- src/main/webapp/components/ActionHandler.js
+++ src/main/webapp/components/ActionHandler.js
@@ -18,28 +18,28 @@ function ActionHandler(e)
 			case HOME:
 				var siblings = $(this).siblings("tr");
 				if (siblings.length !== 0
-						&& siblings[0].getAttribute("tabindex"))
+					&& siblings[0].getAttribute("tabindex"))
 					siblings[0].focus();
 				return false;
 
 			case END:
 				var siblings = $(this).siblings("tr");
 				if (siblings.length !== 0
-						&& siblings[siblings.length - 1].getAttribute("tabindex"))
+					&& siblings[siblings.length - 1].getAttribute("tabindex"))
 					siblings[siblings.length - 1].focus();
 				return false;
 
 			case UP:
 				var prev = $(this).getPrev();
 				if (prev &&
-						prev.getAttribute("tabindex"))
+					prev.getAttribute("tabindex"))
 					prev.focus();
 				return false;
 
 			case DOWN:
 				var next = $(this).getNext();
 				if (next &&
-						next.getAttribute("tabindex"))
+					next.getAttribute("tabindex"))
 					next.focus();
 				return false;
 
@@ -54,27 +54,27 @@ function ActionHandler(e)
 			this.blur();
 			e = e || window.event;
 			for (var parent = e.target || e.srcElement;
-					parent !== this;
-					parent = parent.parentNode)
+				parent !== this;
+				parent = parent.parentNode)
 				if (parent.onclick
-						|| parent.tagName.toLowerCase() === 'a'
-						|| parent.tagName.toLowerCase() === 'button')
+					|| parent.tagName.toLowerCase() === 'a'
+					|| parent.tagName.toLowerCase() === 'button')
 					return;
 			switch (this.getAttribute("data-method") ?
-					this.getAttribute("data-method")
-					.toLowerCase() : "get")
+				this.getAttribute("data-method")
+				.toLowerCase() : "get")
 			{
 				case "get":
 					var a = new Link(document.createElement("a"))
-							.setAction(this.getAttribute("data-action"))
-							.setTarget(e.ctrlKey ? "_blank" : this.getAttribute("data-target"))
-							.setTitle(this.getAttribute("title"))
-							.setOnHide(this.getAttribute("data-onHide"))
-							.setBlock(this.getAttribute("data-block"))
-							.setAlert(this.getAttribute("data-alert"))
-							.setConfirm(this.getAttribute("data-confirm"))
-							.setNavigator(this.getAttribute("data-navigator"))
-							.get();
+						.setAction(this.getAttribute("data-action"))
+						.setTarget(e.ctrlKey ? "_blank" : this.getAttribute("data-target"))
+						.setTitle(this.getAttribute("title"))
+						.setOnHide(this.getAttribute("data-onHide"))
+						.setBlock(this.getAttribute("data-block"))
+						.setAlert(this.getAttribute("data-alert"))
+						.setConfirm(this.getAttribute("data-confirm"))
+						.setNavigator(this.getAttribute("data-navigator"))
+						.get();
 					document.body.appendChild(a);
 					a.click();
 					document.body.removeChild(a);
@@ -82,15 +82,15 @@ function ActionHandler(e)
 				case "post":
 					var form = $(this).getForm();
 					var button = new Button(document.createElement("button"))
-							.setAction(this.getAttribute("data-action"))
-							.setTarget(e.ctrlKey ? "_blank" : this.getAttribute("data-target"))
-							.setTitle(this.getAttribute("title"))
-							.setOnHide(this.getAttribute("data-onHide"))
-							.setBlock(this.getAttribute("data-block"))
-							.setAlert(this.getAttribute("data-alert"))
-							.setConfirm(this.getAttribute("data-confirm"))
-							.setNavigator(this.getAttribute("data-navigator"))
-							.get();
+						.setAction(this.getAttribute("data-action"))
+						.setTarget(e.ctrlKey ? "_blank" : this.getAttribute("data-target"))
+						.setTitle(this.getAttribute("title"))
+						.setOnHide(this.getAttribute("data-onHide"))
+						.setBlock(this.getAttribute("data-block"))
+						.setAlert(this.getAttribute("data-alert"))
+						.setConfirm(this.getAttribute("data-confirm"))
+						.setNavigator(this.getAttribute("data-navigator"))
+						.get();
 					form.appendChild(button);
 					button.click();
 					form.removeChild(button);
