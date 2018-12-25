7a0ceb8cfa41d57a481335320402149079d4c08f
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2018-12-25 19:53:38 -0300

    Implement empty attribute on label tag and make it a html label

--- src/main/java/gate/tags/formControls/LabelTag.java
+++ src/main/java/gate/tags/formControls/LabelTag.java
@@ -7,10 +7,33 @@ import javax.servlet.jsp.JspException;
 public class LabelTag extends PropertyTag
 {
 
+	private String format;
+	private String empty;
+
 	@Override
 	public void doTag() throws JspException, IOException
 	{
 		super.doTag();
-		getJspContext().getOut().print(Converter.toText(getValue()));
+
+		String string = format != null
+			? Converter.toText(getValue(), format)
+			: Converter.toText(getValue());
+		if (string.isEmpty() && empty != null)
+			string = empty;
+		string = string.replaceAll("\\n", "<br/>");
+
+		getJspContext().getOut().println("<label>");
+		getJspContext().getOut().println(Converter.toText(string));
+		getJspContext().getOut().println("</label>");
+	}
+
+	public void setFormat(String format)
+	{
+		this.format = format;
+	}
+
+	public void setEmpty(String emptyText)
+	{
+		this.empty = emptyText;
 	}
 }
