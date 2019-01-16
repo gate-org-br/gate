2a9f1ab10cb3a75b98e24e7082e79b45a90cb61f
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2019-01-15 22:39:54 -0300

    Move styles and scripts to library

--- src/main/resources/gate/Gate.js
+++ src/main/resources/gate/Gate.js
@@ -3951,6 +3951,348 @@ window.addEventListener("load", function ()
 		});
 	});
 });
+/* global DateFormat */
+
+class IconPicker extends Picker
+{
+	constructor(callback)
+	{
+		super();
+
+		this.main().classList.add("IconPicker");
+		this.head().appendChild(document.createTextNode("Selecione um ícone"));
+
+		var selector = this.body()
+			.appendChild(document.createElement("div"));
+
+		icon("1000");
+		icon("1001");
+		icon("1002");
+		icon("1003");
+		icon("1006");
+		icon("1007");
+		icon("1010");
+		icon("1011");
+		icon("1012");
+		icon("1013");
+		icon("1014");
+		icon("1020");
+		icon("1021");
+		icon("1022");
+		icon("1023");
+		icon("1024");
+		icon("2000");
+		icon("2001");
+		icon("2002");
+		icon("2003");
+		icon("2004");
+		icon("2005");
+		icon("2006");
+		icon("2007");
+		icon("2008");
+		icon("2009");
+		icon("200A");
+		icon("2010");
+		icon("2011");
+		icon("2012");
+		icon("2013");
+		icon("2014");
+		icon("2015");
+		icon("2016");
+		icon("2017");
+		icon("2018");
+		icon("2019");
+		icon("2020");
+		icon("2021");
+		icon("2022");
+		icon("2023");
+		icon("2024");
+		icon("2025");
+		icon("2026");
+		icon("2027");
+		icon("2030");
+		icon("2031");
+		icon("2032");
+		icon("2033");
+		icon("2034");
+		icon("2035");
+		icon("2036");
+		icon("2037");
+		icon("2038");
+		icon("2039");
+		icon("2040");
+		icon("2041");
+		icon("2042");
+		icon("2043");
+		icon("2044");
+		icon("2045");
+		icon("2046");
+		icon("2047");
+		icon("2048");
+		icon("2049");
+		icon("2050");
+		icon("2051");
+		icon("2052");
+		icon("2053");
+		icon("2054");
+		icon("2055");
+		icon("2056");
+		icon("2057");
+		icon("2058");
+		icon("2059");
+		icon("2070");
+		icon("2071");
+		icon("2072");
+		icon("2073");
+		icon("2074");
+		icon("2075");
+		icon("2076");
+		icon("2077");
+		icon("2078");
+		icon("2079");
+		icon("2080");
+		icon("2081");
+		icon("2082");
+		icon("2083");
+		icon("2084");
+		icon("2085");
+		icon("2086");
+		icon("2087");
+		icon("2088");
+		icon("2089");
+		icon("2090");
+		icon("2091");
+		icon("2092");
+		icon("2093");
+		icon("2094");
+		icon("2095");
+		icon("2096");
+		icon("2097");
+		icon("2098");
+		icon("2099");
+		icon("2100");
+		icon("2101");
+		icon("2102");
+		icon("2103");
+		icon("2104");
+		icon("2105");
+		icon("2106");
+		icon("2107");
+		icon("2108");
+		icon("2109");
+		icon("2110");
+		icon("2111");
+		icon("2112");
+		icon("2113");
+		icon("2114");
+		icon("2115");
+		icon("2116");
+		icon("2117");
+		icon("2118");
+		icon("2119");
+		icon("2120");
+		icon("2121");
+		icon("2122");
+		icon("2123");
+		icon("2124");
+		icon("2125");
+		icon("2126");
+		icon("2127");
+		icon("2128");
+		icon("2129");
+		icon("2130");
+		icon("2131");
+		icon("2132");
+		icon("2133");
+		icon("2134");
+		icon("2135");
+		icon("2136");
+		icon("2137");
+		icon("2138");
+		icon("2139");
+		icon("2140");
+		icon("2141");
+		icon("2142");
+		icon("2143");
+		icon("2144");
+		icon("2145");
+		icon("2146");
+		icon("2147");
+		icon("2148");
+		icon("2149");
+		icon("2150");
+		icon("2151");
+		icon("2152");
+		icon("2153");
+		icon("2154");
+		icon("2155");
+		icon("2156");
+		icon("2157");
+		icon("2158");
+		icon("2159");
+		icon("2160");
+		icon("2161");
+		icon("2162");
+		icon("2163");
+		icon("2164");
+		icon("2165");
+		icon("2166");
+		icon("2167");
+		icon("2168");
+		icon("2169");
+		icon("2170");
+		icon("2171");
+		icon("2172");
+		icon("2173");
+		icon("2174");
+		icon("2175");
+		icon("2176");
+		icon("2177");
+		icon("2178");
+		icon("2179");
+		icon("2180");
+		icon("2181");
+		icon("2182");
+		icon("2183");
+		icon("2187");
+		icon("2188");
+		icon("2189");
+		icon("2190");
+		icon("2191");
+		icon("2192");
+		icon("2193");
+		icon("2194");
+		icon("2195");
+		icon("2196");
+		icon("2197");
+		icon("2198");
+		icon("2199");
+		icon("2200");
+		icon("2201");
+		icon("2202");
+		icon("2203");
+		icon("2204");
+		icon("2205");
+		icon("2206");
+		icon("2207");
+		icon("2208");
+		icon("2209");
+		icon("2210");
+		icon("2211");
+		icon("2212");
+		icon("2213");
+		icon("2214");
+		icon("2215");
+		icon("2216");
+		icon("2217");
+		icon("2218");
+		icon("2219");
+		icon("2220");
+		icon("2221");
+		icon("2222");
+		icon("2223");
+		icon("2224");
+		icon("2225");
+		icon("2226");
+		icon("2227");
+		icon("2228");
+		icon("2229");
+		icon("2230");
+		icon("2231");
+		icon("2232");
+		icon("2233");
+		icon("2234");
+		icon("2235");
+		icon("2236");
+		icon("2237");
+		icon("2238");
+		icon("2239");
+		icon("2240");
+		icon("2241");
+		icon("2242");
+		icon("2243");
+		icon("2244");
+		icon("2245");
+		icon("2246");
+		icon("2247");
+		icon("2248");
+		icon("2249");
+		icon("2250");
+		icon("2251");
+		icon("2252");
+		icon("2253");
+		icon("2254");
+		icon("2255");
+		icon("2256");
+		icon("2257");
+		icon("2258");
+		icon("2259");
+		icon("2260");
+		icon("2261");
+		icon("2262");
+		icon("2263");
+		icon("2264");
+		icon("2265");
+		icon("2266");
+		icon("2267");
+		icon("2268");
+		icon("2269");
+		icon("2270");
+		icon("2271");
+		icon("2272");
+		icon("2273");
+		icon("2274");
+		icon("2275");
+		icon("2276");
+		icon("2277");
+		icon("2278");
+		icon("2279");
+		icon("2280");
+		icon("2281");
+		icon("2282");
+		icon("2283");
+
+		this.commit().addEventListener("click", () => alert("selecione um ícone"));
+
+		this.show();
+
+		var element = this;
+		function icon(code)
+		{
+			var icon = selector.appendChild(document.createElement("a"));
+			icon.innerHTML = "&#X" + code + ";";
+			icon.href = "#";
+			icon.addEventListener("click", () => callback(code) | element.hide());
+		}
+	}
+}
+
+window.addEventListener("load", function ()
+{
+	Array.from(document.querySelectorAll("input.Icon")).forEach(function (input)
+	{
+		var link = input.parentNode.appendChild(document.createElement("a"));
+		link.href = "#";
+		link.setAttribute("tabindex", input.getAttribute('tabindex'));
+		link.appendChild(document.createElement("i")).innerHTML = "&#x2009;";
+
+		link.addEventListener("click", function (event)
+		{
+			event.preventDefault();
+
+			if (input.value)
+				input.value = '';
+			else
+				new IconPicker(code =>
+				{
+					input.value = code;
+					link.focus();
+				});
+
+			link.blur();
+		});
+	});
+});
 function Chart(data, title)
 {
 	this.categories = new Array();
@@ -4209,149 +4551,97 @@ function Chart(data, title)
 	};
 }
 
-function ChartDialog(data, type, title)
+class ChartDialog extends Modal
 {
-	var modal = new Modal(true);
-
-	var dialog = modal.element().appendChild(window.top.document.createElement('div'));
-	dialog.className = "Dialog";
-
-	var head = dialog.appendChild(document.createElement("div"));
-	head.setAttribute("tabindex", "1");
-
-	var caption = head.appendChild(window.top.document.createElement('label'));
-	caption.innerHTML = title;
-	caption.style['float'] = "left";
-
-	var body = dialog.appendChild(document.createElement("div"));
-	body.style.backgroundColor = "white";
-
-	var canvas = body.appendChild(document.createElement("div"));
-	canvas.setAttribute("id", 'Chart');
-	canvas.setAttribute("tabindex", "1");
-
-	canvas.onmouseenter = function ()
+	constructor(options)
 	{
-		this.focus();
-	};
+		super(options);
 
-	head.onmouseenter = function ()
-	{
-		this.focus();
-	};
+		var dialog = this.element().appendChild(window.top.document.createElement('div'));
+		dialog.classList.add("ChartDialog");
 
-	var close = head.appendChild(document.createElement('a'));
-	close.style['float'] = "right";
-	close.dialog = this;
-	close.title = "Fechar";
-	close.innerHTML = "&#x1011;";
-	close.style.marginLeft = '16px';
-	close.style.fontFamily = "gate";
-	close.onclick = function ()
-	{
-		modal.hide();
-	};
+		var head = dialog.appendChild(document.createElement("div"));
+		head.setAttribute("tabindex", "1");
 
-	var chart = new Chart(data, title);
+		var body = dialog.appendChild(document.createElement("div"));
+		body.style.backgroundColor = "white";
 
-	var rchart = head.appendChild(document.createElement('a'));
-	rchart.style['float'] = "right";
-	rchart.dialog = this;
-	rchart.title = "Rose";
-	rchart.innerHTML = "&#x2247;";
-	rchart.style.fontFamily = "gate";
-	rchart.onclick = function ()
-	{
-		chart.draw('Chart', 'rchart');
-	};
+		var canvas = body.appendChild(document.createElement("div"));
+		canvas.setAttribute("id", 'Chart');
+		canvas.setAttribute("tabindex", "1");
 
-	var dchart = head.appendChild(document.createElement('a'));
-	dchart.style['float'] = "right";
-	dchart.dialog = this;
-	dchart.title = "Donut";
-	dchart.innerHTML = "&#x2245;";
-	dchart.style.fontFamily = "gate";
-	dchart.onclick = function ()
-	{
-		chart.draw('Chart', 'dchart');
-	};
+		canvas.onmouseenter = () => canvas.focus();
+		head.onmouseenter = () => head.focus();
 
-	var pchart = head.appendChild(document.createElement('a'));
-	pchart.style['float'] = "right";
-	pchart.dialog = this;
-	pchart.title = "Pizza";
-	pchart.innerHTML = "&#x2031;";
-	pchart.style.fontFamily = "gate";
-	pchart.onclick = function ()
-	{
-		chart.draw('Chart', 'pchart');
-	};
+		var chart = new Chart(options.data, options.title ? options.title : "");
 
-	var achart = head.appendChild(document.createElement('a'));
-	achart.style['float'] = "right";
-	achart.dialog = this;
-	achart.title = "Area";
-	achart.innerHTML = "&#x2244;";
-	achart.style.fontFamily = "gate";
-	achart.onclick = function ()
-	{
-		chart.draw('Chart', 'achart');
-	};
+		var cchart = head.appendChild(document.createElement('a'));
+		cchart.dialog = this;
+		cchart.title = "Coluna";
+		cchart.innerHTML = "&#x2033;";
+		cchart.onclick = () => chart.draw('Chart', 'cchart');
 
-	var lchart = head.appendChild(document.createElement('a'));
-	lchart.style['float'] = "right";
-	lchart.dialog = this;
-	lchart.title = "Linha";
-	lchart.innerHTML = "&#x2032;";
-	lchart.style.fontFamily = "gate";
-	lchart.onclick = function ()
-	{
-		chart.draw('Chart', 'lchart');
-	};
+		var bchart = head.appendChild(document.createElement('a'));
+		bchart.dialog = this;
+		bchart.title = "Barra";
+		bchart.innerHTML = "&#x2246;";
+		bchart.onclick = () => chart.draw('Chart', 'bchart');
 
-	var bchart = head.appendChild(document.createElement('a'));
-	bchart.style['float'] = "right";
-	bchart.dialog = this;
-	bchart.title = "Barra";
-	bchart.innerHTML = "&#x2246;";
-	bchart.style.fontFamily = "gate";
-	bchart.onclick = function ()
-	{
-		chart.draw('Chart', 'bchart');
-	};
+		var lchart = head.appendChild(document.createElement('a'));
+		lchart.dialog = this;
+		lchart.title = "Linha";
+		lchart.innerHTML = "&#x2032;";
+		lchart.onclick = () => chart.draw('Chart', 'lchart');
 
-	var cchart = head.appendChild(document.createElement('a'));
-	cchart.style['float'] = "right";
-	cchart.dialog = this;
-	cchart.title = "Coluna";
-	cchart.innerHTML = "&#x2033;";
-	cchart.style.fontFamily = "gate";
-	cchart.onclick = function ()
-	{
-		chart.draw('Chart', 'cchart');
-	};
+		var achart = head.appendChild(document.createElement('a'));
+		achart.dialog = this;
+		achart.title = "Area";
+		achart.innerHTML = "&#x2244;";
+		achart.onclick = () => chart.draw('Chart', 'achart');
 
-	modal.show();
+		var pchart = head.appendChild(document.createElement('a'));
+		pchart.dialog = this;
+		pchart.title = "Pizza";
+		pchart.innerHTML = "&#x2031;";
+		pchart.onclick = () => chart.draw('Chart', 'pchart');
 
-	if (!type)
-		type = 'cchart';
-	chart.draw('Chart', type);
+		var dchart = head.appendChild(document.createElement('a'));
+		dchart.dialog = this;
+		dchart.title = "Donut";
+		dchart.innerHTML = "&#x2245;";
+		dchart.onclick = () => chart.draw('Chart', 'dchart');
 
-	modal.onclick = function (e)
-	{
-		if (e.target === this
-			|| e.srcElement === this)
-			modal.hide();
-	};
+		var rchart = head.appendChild(document.createElement('a'));
+		rchart.dialog = this;
+		rchart.title = "Rose";
+		rchart.innerHTML = "&#x2247;";
+		rchart.onclick = () => chart.draw('Chart', 'rchart');
 
-	head.focus();
+		head.appendChild(window.top.document.createElement('span'));
+
+		var close = head.appendChild(document.createElement('a'));
+		close.dialog = this;
+		close.title = "Fechar";
+		close.innerHTML = "&#x1011;";
+		close.style.marginLeft = '16px';
+		close.onclick = () => this.hide();
+
+		this.show();
+
+		if (!options.type)
+			options.type = 'cchart';
+		chart.draw('Chart', options.type);
+
+
+		head.focus();
+	}
 }
 
 ChartDialog.show = function (chart, series, action, title)
 {
 	if (!series)
 		series = new URL(action).get();
-	new ChartDialog(JSON.parse(series), chart, title);
+	new ChartDialog({data: JSON.parse(series), type: chart, title: title});
 };
 
 window.addEventListener("load", function ()
@@ -5152,49 +5442,6 @@ window.addEventListener("load", function ()
 });
 
 
-/* global CKEDITOR */
-
-window.addEventListener("load", function ()
-{
-	Array.from(document.querySelectorAll("textarea.Rich")).forEach(function (e)
-	{
-		CKEDITOR.config.toolbar =
-			[
-				{name: 'Font', items: ['Font', 'FontSize']},
-				{name: 'Format', items: ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript']},
-				{name: 'Color', items: ['TextColor', 'BGColor']},
-				{name: 'Align', items: ['JustifyLeft', 'JustifyCenter', 'JustifyRight', '-', 'JustifyBlock']},
-				{name: 'Identation', items: ['Outdent', 'Indent']},
-				{name: 'Clear', items: ['RemoveFormat']},
-				{name: 'List', items: ['NumberedList', 'BulletedList']},
-				{name: 'Links', items: ['Link', 'Unlink']},
-				{name: 'Insert', items: ['HorizontalRule', 'Table', 'Smiley']},
-				{name: 'Tools', items: ['Maximize']}
-			];
-
-		var tabindex = e.getAttribute("tabindex");
-		if (tabindex)
-		{
-			e.removeAttribute("tabindex");
-			CKEDITOR.config.tabIndex = tabindex;
-		}
-
-		if (e.getAttribute("autofocus"))
-			e.removeAttribute("autofocus");
-
-		if (document.activeElement === e)
-			CKEDITOR.config.startupFocus = true;
-
-		CKEDITOR.config.language = 'pt-br';
-		CKEDITOR.config.allowedContent = true;
-		CKEDITOR.config.extraPlugins = 'maximize';
-		CKEDITOR.config.width = "100%";
-		CKEDITOR.config.height = e.parentNode.clientHeight + "px";
-		CKEDITOR.config.resize_enabled = false;
-		CKEDITOR.config.contentsCss = '../gate/Gate.css';
-		CKEDITOR.replace(e.getAttribute("id"));
-	});
-});
 window.addEventListener("load", function ()
 {
 	document.addEventListener("fullscreenchange", function ()
