e71b40ed3af9f5ddd381c3652153baf08df02718
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2019-01-17 08:44:36 -0300

    Implement property support for primitive arrays

--- src/main/java/gate/lang/property/ArrayAttribute.java
+++ src/main/java/gate/lang/property/ArrayAttribute.java
@@ -48,7 +48,7 @@ class ArrayAttribute implements Attribute
 			return null;
 		Object[] array = ((Object[]) object);
 		if (array.length > index
-				&& array[index] == null)
+			&& array[index] == null)
 			array[index] = createInstance(getElementRawType());
 		return array[index];
 	}
@@ -60,11 +60,107 @@ class ArrayAttribute implements Attribute
 	}
 
 	@Override
+	public boolean getBoolean(Object object)
+	{
+		return object != null ? ((boolean[]) object)[index] : false;
+	}
+
+	@Override
+	public void setBoolean(Object object, boolean value)
+	{
+		((boolean[]) object)[index] = value;
+	}
+
+	@Override
+	public char getChar(Object object)
+	{
+		return object != null ? ((char[]) object)[index] : Character.MIN_VALUE;
+	}
+
+	@Override
+	public void setChar(Object object, char value)
+	{
+		((char[]) object)[index] = value;
+	}
+
+	@Override
+	public byte getByte(Object object)
+	{
+		return object != null ? ((byte[]) object)[index] : 0;
+	}
+
+	@Override
+	public void setByte(Object object, byte value)
+	{
+		((byte[]) object)[index] = value;
+	}
+
+	@Override
+	public short getShort(Object object)
+	{
+		return object != null ? ((short[]) object)[index] : 0;
+	}
+
+	@Override
+	public void setShort(Object object, short value)
+	{
+		((short[]) object)[index] = value;
+	}
+
+	@Override
+	public int getInt(Object object)
+	{
+		return object != null ? ((int[]) object)[index] : 0;
+	}
+
+	@Override
+	public void setInt(Object object, int value)
+	{
+		((int[]) object)[index] = value;
+	}
+
+	@Override
+	public long getLong(Object object)
+	{
+		return object != null ? ((long[]) object)[index] : 0;
+	}
+
+	@Override
+	public void setLong(Object object, long value)
+	{
+		((long[]) object)[index] = value;
+	}
+
+	@Override
+	public float getFloat(Object object)
+	{
+		return object != null ? ((float[]) object)[index] : 0;
+	}
+
+	@Override
+	public void setFloat(Object object, float value)
+	{
+		((float[]) object)[index] = value;
+	}
+
+	@Override
+	public double getDouble(Object object)
+	{
+		return object != null ? ((double[]) object)[index] : 0;
+	}
+
+	@Override
+	public void setDouble(Object object, double value)
+	{
+		((double[]) object)[index] = value;
+	}
+
+	@Override
 	public boolean equals(Object obj)
 	{
 		return obj instanceof ArrayAttribute
-				&& type.equals(((ArrayAttribute) obj).type)
-				&& index == ((ArrayAttribute) obj).index;
+			&& type.equals(((ArrayAttribute) obj).type)
+			&& index == ((ArrayAttribute) obj).index;
 	}
 
 	@Override
@@ -77,7 +173,7 @@ class ArrayAttribute implements Attribute
 	public String toString()
 	{
 		return new StringBuilder("[")
-				.append(index).append("]")
-				.toString();
+			.append(index).append("]")
+			.toString();
 	}
 }
