9f65789248ceeca1074b549aadede303e3bbaa91
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2019-02-15 09:28:49 -0300

    Remove obsolete Progress component

--- src/main/resources/gate/Gate.js
+++ src/main/resources/gate/Gate.js
@@ -5582,39 +5582,6 @@ window.addEventListener("load", function ()
 
 
 
-function Progress(process)
-{
-	var ws = new WebSocket("ws://" + window.location.host + "/" +
-		window.location.pathname.split("/")[1] + "/Progress/" + process);
-	ws.onmessage = function (event)
-	{
-		event = JSON.parse(event.data);
-
-		event.toString = function ()
-		{
-			if (this.done && this.done !== -1)
-				if (this.todo && this.todo !== -1)
-					return this.done + "/" + this.todo;
-				else
-					return this.done.toString();
-			else
-				return "...";
-		};
-
-		switch (event.event)
-		{
-			case "Progress":
-				for (var i = 0; i < document.all.length; i++)
-					if (document.all[i].onProgressEvent)
-						document.all[i].onProgressEvent(event);
-				break;
-
-			case "Redirect":
-				window.location.href = event.url;
-				break;
-		}
-	};
-}
 function autofocus(d)
 {
 	var elements = Array.from(d.querySelectorAll('[autofocus]'));
