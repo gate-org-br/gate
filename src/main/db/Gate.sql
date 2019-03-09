c265b6f82315db750d8c828ee116fe6e5ced7c8c
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2019-03-09 06:28:12 -0300

    Update script and packages

--- src/main/db/Gate.sql
+++ src/main/db/Gate.sql
@@ -1,8 +1,5 @@
--- MySQL dump 10.14  Distrib 5.5.50-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: 127.0.0.1    Database: gate
--- ------------------------------------------------------
--- Server version	5.5.50-MariaDB-1~trusty
+create database gate;
+use gate;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,55 +17,46 @@
 --
 
 DROP TABLE IF EXISTS `Auth`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Auth` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Role$id` int(10) unsigned DEFAULT NULL,
   `Uzer$id` int(10) unsigned DEFAULT NULL,
+  `Func$id` int(10) unsigned DEFAULT NULL,
   `module` varchar(32) DEFAULT NULL,
   `screen` varchar(32) DEFAULT NULL,
   `action` varchar(32) DEFAULT NULL,
   `type` tinyint(4) NOT NULL DEFAULT '0',
   `mode` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `Auth$fk1_idx` (`Role$id`),
-  KEY `Auth$fk2_idx` (`Uzer$id`),
-  CONSTRAINT `Auth$fk1` FOREIGN KEY (`Role$id`) REFERENCES `Role` (`id`),
-  CONSTRAINT `Auth$fk2` FOREIGN KEY (`Uzer$id`) REFERENCES `Uzer` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=137 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `Auth$fk$Func` (`Func$id`),
+  KEY `Auth$fk$Role` (`Role$id`),
+  KEY `Auth$fk$Uzer` (`Uzer$id`),
+  CONSTRAINT `Auth$fk$Func` FOREIGN KEY (`Func$id`) REFERENCES `Func` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Auth$fk$Role` FOREIGN KEY (`Role$id`) REFERENCES `Role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Auth$fk$Uzer` FOREIGN KEY (`Uzer$id`) REFERENCES `Uzer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `Msg`
+-- Table structure for table `Func`
 --
 
-DROP TABLE IF EXISTS `Msg`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Msg` (
+DROP TABLE IF EXISTS `Func`;
+CREATE TABLE `Func` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `sender` varchar(64) DEFAULT NULL,
-  `receiver` varchar(64) NOT NULL,
-  `dispatch` datetime NOT NULL,
-  `failures` tinyint(2) NOT NULL,
-  `mimeMail` mediumtext NOT NULL,
-  `exception` text,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5379 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Func$uk$name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `Org`
 --
 
 DROP TABLE IF EXISTS `Org`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Org` (
   `orgID` varchar(16) NOT NULL,
   `name` varchar(256) NOT NULL,
-  `icon` longblob NOT NULL,
+  `icon` longblob,
   `description` varchar(256) DEFAULT NULL,
   `authenticators` text,
   `sun$time1` time DEFAULT NULL,
@@ -84,22 +72,19 @@ CREATE TABLE `Org` (
   `fri$time1` time DEFAULT NULL,
   `fri$time2` time DEFAULT NULL,
   `sat$time1` time DEFAULT NULL,
-  `sat$time2` varchar(45) DEFAULT NULL,
+  `sat$time2` time DEFAULT NULL,
   PRIMARY KEY (`orgID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `Role`
 --
 
 DROP TABLE IF EXISTS `Role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Role` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `active` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  `master` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `master` tinyint(1) unsigned NOT NULL,
   `name` varchar(64) NOT NULL,
   `description` varchar(256) DEFAULT NULL,
   `Role$id` int(10) unsigned DEFAULT NULL,
@@ -109,17 +94,30 @@ CREATE TABLE `Role` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `roleID_UNIQUE` (`roleID`),
   KEY `Role$fk1_idx` (`Manager$id`),
-  CONSTRAINT `Role$fk1` FOREIGN KEY (`Manager$id`) REFERENCES `Uzer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `Roke$fk$Role_idx` (`Role$id`),
+  CONSTRAINT `Roke$fk$Role` FOREIGN KEY (`Role$id`) REFERENCES `Role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Role$fk$Uzer` FOREIGN KEY (`Manager$id`) REFERENCES `Uzer` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `RoleFunc`
+--
+
+DROP TABLE IF EXISTS `RoleFunc`;
+CREATE TABLE `RoleFunc` (
+  `Role$id` int(10) unsigned NOT NULL,
+  `Func$id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`Role$id`,`Func$id`),
+  KEY `RoleFunc$fk$Func` (`Func$id`),
+  CONSTRAINT `RoleFunc$fk$Func` FOREIGN KEY (`Func$id`) REFERENCES `Func` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `RoleFunc$fk$Role` FOREIGN KEY (`Role$id`) REFERENCES `Role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Table structure for table `Uzer`
 --
 
 DROP TABLE IF EXISTS `Uzer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Uzer` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `active` tinyint(1) unsigned NOT NULL DEFAULT '1',
@@ -130,14 +128,32 @@ CREATE TABLE `Uzer` (
   `email` varchar(64) DEFAULT NULL,
   `details` varchar(1024) DEFAULT NULL,
   `phone` varchar(24) DEFAULT NULL,
-  `cellPhone` varchar(24) DEFAULT NULL,
+  `cellPhone` varchar(45) DEFAULT NULL,
+  `photo` text,
+  `CPF` char(14) DEFAULT NULL,
+  `sex` tinyint(1) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `registration` date NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_Uzer_userID` (`userID`),
-  KEY `Uzer$fk1_idx` (`Role$id`),
-  CONSTRAINT `Uzer$fk1` FOREIGN KEY (`Role$id`) REFERENCES `Role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  UNIQUE KEY `Uzer$uk$userID` (`userID`),
+  UNIQUE KEY `Uzer$uk$email` (`email`),
+  KEY `Uzer$fk$Role` (`Role$id`),
+  CONSTRAINT `Uzer$fk$Role` FOREIGN KEY (`Role$id`) REFERENCES `Role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `UzerFunc`
+--
+
+DROP TABLE IF EXISTS `UzerFunc`;
+CREATE TABLE `UzerFunc` (
+  `Uzer$id` int(10) unsigned NOT NULL,
+  `Func$id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`Uzer$id`,`Func$id`),
+  KEY `UzerFunc$fk$Func` (`Func$id`),
+  CONSTRAINT `UzerFunc$fk$Func` FOREIGN KEY (`Func$id`) REFERENCES `Func` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `UzerFunc$fk$Uzer` FOREIGN KEY (`Uzer$id`) REFERENCES `Uzer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -147,4 +163,8 @@ CREATE TABLE `Uzer` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-02  7:32:00
+INSERT INTO `gate`.`Func` (`id`, `name`) VALUES ('1', 'Superuser');
+INSERT INTO `gate`.`Role` (`active`, `master`, `name`, `roleID`) VALUES ('1', '1', 'Gate', 'gate');
+INSERT INTO `gate`.`Uzer` (active, Role$id, userID, passwd, name, registration) VALUES(1, 1, 'gate', MD5('password'), 'Superuser', now());
+INSERT INTO `gate`.`UzerFunc` (`Uzer$id`, `Func$id`) VALUES ('1', '1');
+INSERT INTO `gate`.`Auth` (`Func$id`, `type`, `mode`) VALUES ('1', '1', '0');
