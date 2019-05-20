221bb6f94be675d3e569b023aaef4f72d66ac83c
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2019-05-20 08:48:15 -0300

    Implement a MonthOfYear type

---
+++ src/main/java/gate/type/MonthOfYear.java
@@ -0,0 +1,33 @@
+package gate.type;
+
+import gate.annotation.Icon;
+import gate.annotation.Name;
+
+@Icon("2009")
+public enum MonthOfYear
+{
+	@Name("Janeiro")
+	JANUARY,
+	@Name("Fevereiro")
+	FEBRUARY,
+	@Name("Março")
+	MARCH,
+	@Name("Abril")
+	APRIL,
+	@Name("Maio")
+	MAY,
+	@Name("Junho")
+	JUNE,
+	@Name("Julho")
+	JULY,
+	@Name("Agosto")
+	AUGUST,
+	@Name("Setembro")
+	SEPTEMBER,
+	@Name("Outubro")
+	OCTOBER,
+	@Name("Novembro")
+	NOVEMBER,
+	@Name("Dezembro")
+	DECEMBER;
+}
