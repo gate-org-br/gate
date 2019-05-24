fa8231a671545b8e68735b0d334a87a98288289f
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2019-05-23 21:19:41 -0300

    Fix issue when displaying stripped tables

--- src/main/webapp/components/Colorizer.js
+++ src/main/webapp/components/Colorizer.js
@@ -9,7 +9,7 @@ class Colorizer
 		{
 			row.classList.remove("odd");
 			row.classList.remove("even");
-			if (row.style.display !== "none")
+			if (window.getComputedStyle(row).display !== "none")
 			{
 				row.classList.add(type);
 				type = type === "even" ? "odd" : "even";
