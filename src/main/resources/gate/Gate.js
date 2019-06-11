a41c98d54fffa571c7677d0c6e50fdb88ce483b1
davinunesdasilva <34130898+davinunesdasilva@users.noreply.github.com>
2019-06-11 13:39:44 -0300

    Fix scroll error

--- src/main/resources/gate/Gate.js
+++ src/main/resources/gate/Gate.js
@@ -6714,27 +6714,9 @@ class Coolbar extends HTMLElement
 	{
 		super();
 	}
-
-	static resize()
-	{
-		Array.from(document.querySelectorAll('cool-bar, div.COOLBAR, div.Coolbar'))
-			.forEach(function (element)
-			{
-				element.style.visibility = "hidden";
-				element.removeAttribute("data-overflow");
-				if (element.scrollWidth > element.clientWidth
-					|| element.scrollHeight > element.clientHeight)
-					element.setAttribute("data-overflow", "true");
-				element.style.visibility = "visible";
-			});
-	}
 }
 
 customElements.define('cool-bar', Coolbar);
-
-window.addEventListener("load", () => Coolbar.resize());
-window.addEventListener("resize", () => Coolbar.resize());
-
 
 window.addEventListener("load", () => Array.from(document.all)
 		.filter(e => e.scrollWidth > e.clientWidth
