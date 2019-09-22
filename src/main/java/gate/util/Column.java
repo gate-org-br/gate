3195724ab7193700534928d62b03fcd261c634c0
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2019-09-22 16:18:05 -0300

    Improve Column extractor API

--- src/main/java/gate/util/Column.java
+++ src/main/java/gate/util/Column.java
@@ -1,19 +1,90 @@
 package gate.util;
 
-public class Column
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+/**
+ * Used to extract data from strings
+ *
+ * @param <T> the java type of the data to be extracted
+ */
+public interface Column<T>
 {
 
-	private final int min;
-	private final int max;
+	public abstract T get(String string);
 
-	public Column(int min, int max)
+	/**
+	 * Create a column to extract data from a string and return it as a java object
+	 *
+	 * @param min column start position
+	 * @param max column end position
+	 *
+	 * @return A java string representing the content of the extracted data or null if the data is blank
+	 */
+	public static Column<String> of(int min, int max)
 	{
-		this.min = min;
-		this.max = max;
+		return e ->
+		{
+			String value = e.substring(min, max).trim();
+			return !value.isEmpty() ? value : null;
+		};
+
 	}
 
-	public String get(String string)
+	/**
+	 * Create a column to extract data from a string and return it as a java object
+	 *
+	 * @param <T> java type of the column to be extracted
+	 * @param min column start position
+	 * @param max column end position
+	 * @param converter function to be used to converted the extracted string to a java object if the extracted string is not blank
+	 *
+	 * @return A java object representing the content of the extracted data or null if the data is blank
+	 */
+	public static <T> Column<T> of(int min, int max, Function<String, T> converter)
 	{
-		return string.substring(min, max).trim();
+		return e ->
+		{
+			String value = e.substring(min, max).trim();
+			return !value.isEmpty() ? converter.apply(value) : null;
+		};
+	}
+
+	/**
+	 * Create a column to extract data from a string and return it as a string
+	 *
+	 * @param min column start position
+	 * @param max column end position
+	 * @param cleanup used to remove characters from the extracted string
+	 *
+	 * @return A java string representing the content of the extracted data or null if the data is blank
+	 */
+	public static Column<String> of(int min, int max, Pattern cleanup)
+	{
+		return e ->
+		{
+			String value = cleanup.matcher(e.substring(min, max)).replaceAll("");
+			return !value.isEmpty() ? value : null;
+		};
+	}
+
+	/**
+	 * Create a column to extract data from a string and return it as a java object
+	 *
+	 * @param <T> java type of the column to be extracted
+	 * @param min column start position
+	 * @param max column end position
+	 * @param cleanup used to remove characters from the extracted string before conversion to a java object
+	 * @param converter function to be used to converted the extracted string to a java object if the extracted string is not blank
+	 *
+	 * @return A java object representing the content of the extracted data or null if the data is blank
+	 */
+	public static <T> Column<T> of(int min, int max, Pattern cleanup, Function<String, T> converter)
+	{
+		return e ->
+		{
+			String value = cleanup.matcher(e.substring(min, max)).replaceAll("");
+			return !value.isEmpty() ? converter.apply(value) : null;
+		};
 	}
 }
