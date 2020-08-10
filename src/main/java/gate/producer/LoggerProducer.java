98a905d486e4ef46ff3f88d51f6d9e5abf095f30
davins <davins@davins>
2020-08-10 04:29:00 -0300

    Implement a sl4j logger produccer

---
+++ src/main/java/gate/producer/LoggerProducer.java
@@ -0,0 +1,17 @@
+package gate.producer;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerProducer
+{
+
+	@Produces
+	public Logger produceLogger(InjectionPoint injectionPoint)
+	{
+		return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
+	}
+
+}
