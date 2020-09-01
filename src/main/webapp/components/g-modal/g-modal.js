1426902ee5b943a079f0de33c8a21d8721284067
davins <davins@davins>
2020-09-01 19:25:36 -0300

    Fix error when opening modals on older browsers with polyfill

--- src/main/webapp/components/g-modal/g-modal.js
+++ src/main/webapp/components/g-modal/g-modal.js
@@ -40,8 +40,7 @@ class GModal extends HTMLElement
 	show()
 	{
 
-		if (window.dispatchEvent(new CustomEvent('show', {cancelable: true, detail: {modal: this}}))
-			&& this.dispatchEvent(new CustomEvent('show', {cancelable: true, detail: {modal: this}})))
+		if (window.dispatchEvent(new CustomEvent('show', {cancelable: true, detail: {modal: this}})))
 		{
 			GOverflow.disable(window.top.document.documentElement);
 
