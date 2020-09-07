152e4809bab8f6f8a9271bb339b9a6dd58e93cf5
davins <davins@davins>
2020-09-07 15:55:28 -0300

    Implement a fullname function on db script

--- src/main/db/Gate.sql
+++ src/main/db/Gate.sql
@@ -182,6 +182,41 @@ CREATE TABLE `UzerFunc` (
 --
 -- Dumping routines for database 'gate'
 --
+/*!50003 DROP FUNCTION IF EXISTS `fullname` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `fullname`(parameter integer) RETURNS text CHARSET utf8mb4
+    READS SQL DATA
+    DETERMINISTIC
+BEGIN
+	declare fullname  text;
+	declare parent integer;
+	declare simplename varchar(64);
+        
+  	SELECT Role$id, coalesce(roleID, name) INTO parent, fullname FROM Role WHERE  id = parameter;
+    
+	REPEAT
+		SELECT Role$id, coalesce(roleID, name) INTO parent, simplename FROM Role WHERE  id = parent;
+		IF simplename is not null THEN 
+			set fullname = concat(simplename, ' / ', fullname);
+		END IF;
+	UNTIL parent is null
+	END REPEAT;
+    
+	return fullname;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP FUNCTION IF EXISTS `ismaster` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -256,4 +291,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-06 13:13:30
+-- Dump completed on 2020-09-07 15:54:18
