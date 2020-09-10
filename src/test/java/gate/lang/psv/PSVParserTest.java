14c78b905e06ea922d8602784377808fac589f24
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2020-09-10 19:43:22 -0300

    Implements a psv parser

---
+++ src/test/java/gate/lang/psv/PSVParserTest.java
@@ -0,0 +1,31 @@
+package gate.lang.psv;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PSVParserTest
+{
+
+	@Test
+	public void testUsers1() throws IOException
+	{
+		try (PSVParser parser = new PSVParser(getClass().getResource("Users1.dat"), 0, 6, 12, 12, 12, 12, 12))
+		{
+
+			List<List<String>> lines = parser.stream().collect(Collectors.toList());
+			Assert.assertEquals(7, lines.size());
+
+			int index = 0;
+			Assert.assertEquals(List.of("User 1", "Role 1", "", "", "", "", "Name 1"), lines.get(index++));
+			Assert.assertEquals(List.of("User 2", "Role 2", "", "", "", "", "Name 2"), lines.get(index++));
+			Assert.assertTrue(lines.get(index++).isEmpty());
+			Assert.assertEquals(List.of("User 3", "Role 3", "", "", "", "", "Name 3"), lines.get(index++));
+			Assert.assertTrue(lines.get(index++).isEmpty());
+			Assert.assertTrue(lines.get(index++).isEmpty());
+			Assert.assertEquals(List.of("User 4", "Role 4", "", "", "", "", "Name 4"), lines.get(index++));
+		}
+	}
+}
