b182b5b343de81c20fd6d265a7a9c945cf5ea62b
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2020-09-12 15:04:33 -0300

    Implement some convenient static factory methods

--- src/main/java/gate/lang/csv/Row.java
+++ src/main/java/gate/lang/csv/Row.java
@@ -1,6 +1,8 @@
 package gate.lang.csv;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Row extends ArrayList<String>
 {
@@ -32,5 +34,18 @@ public class Row extends ArrayList<String>
 	public String getText()
 	{
 		return text;
+	}
+
+	/**
+	 * Creates a row with the specified index, text and values
+	 *
+	 * @param index the index of the new row
+	 * @param text the text of the new row
+	 * @param values the values of the new row
+	 * @return a new row with the specified index, text and values
+	 */
+	public static Row of(long index, String text, String... values)
+	{
+		return Stream.of(values).collect(Collectors.toCollection(() -> new Row(index, text)));
 	}
 }
