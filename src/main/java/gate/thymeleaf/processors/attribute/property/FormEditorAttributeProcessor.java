c1dcd0b782bc37fc374ecd996785f4239d745808
davins <davins@davins>
2022-12-31 06:27:54 -0300

    Fix error when parsing property names

---
+++ src/main/java/gate/thymeleaf/processors/attribute/property/FormEditorAttributeProcessor.java
@@ -0,0 +1,32 @@
+package gate.thymeleaf.processors.attribute.property;
+
+import gate.base.Screen;
+import gate.converter.Converter;
+import gate.lang.property.Property;
+import gate.thymeleaf.ELExpression;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.model.AttributeValueQuotes;
+import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.processor.element.IElementTagStructureHandler;
+
+@ApplicationScoped
+public class FormEditorAttributeProcessor extends FormControlAttributeProcessor
+{
+
+	@Inject
+	ELExpression expression;
+
+	public FormEditorAttributeProcessor()
+	{
+		super("g-form-editor");
+	}
+
+	@Override
+	public void process(ITemplateContext context, IProcessableElementTag element,
+		IElementTagStructureHandler handler, Screen screen, Property property, Object value)
+	{
+		handler.setAttribute("value", Converter.toString(value), AttributeValueQuotes.SINGLE);
+	}
+}
