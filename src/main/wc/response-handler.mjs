4cdb40dab209c4b144d95cb9822fa37480aad8dd
Davi Nunes da Silva <davimunesdasilva@gmail.com>
2023-05-11 06:20:01 -0300

    Implement response-handler documentation

--- src/main/wc/response-handler.mjs
+++ src/main/wc/response-handler.mjs
@@ -1,7 +1,17 @@
-/* global Promise */
-
+/**
+ * Utility class for handling responses from HTTP requests.
+ */
 export default class ResponseHandler
 {
+
+	/**
+	 * Returns a promise that resolves to the JSON body of the response,
+	 * or rejects with an error message if the response is not successful.
+	 *
+	 * @param {Response} response - The response object to handle.
+	 * @returns {Promise} A promise that resolves to the JSON body of the response,
+	 *                    or rejects with an error message.
+	 */
 	static json(response)
 	{
 		if (!response)
@@ -10,10 +20,17 @@ export default class ResponseHandler
 		if (response.ok)
 			return response.json();
 
-		return response.text()
-			.then(error => Promise.reject(new Error(error)));
+		return response.text().then((error) => Promise.reject(new Error(error)));
 	}
 
+	/**
+	 * Returns a promise that resolves to the text body of the response,
+	 * or rejects with an error message if the response is not successful.
+	 *
+	 * @param {Response} response - The response object to handle.
+	 * @returns {Promise} A promise that resolves to the text body of the response,
+	 *                    or rejects with an error message.
+	 */
 	static text(response)
 	{
 		if (!response)
@@ -22,11 +39,18 @@ export default class ResponseHandler
 		if (response.ok)
 			return response.text();
 
-		return response.text()
-			.then(error => Promise.reject(new Error(error)));
+		return response.text().then((error) => Promise.reject(new Error(error)));
 	}
 
-	static 	blob(response)
+	/**
+	 * Returns a promise that resolves to the blob body of the response,
+	 * or rejects with an error message if the response is not successful.
+	 *
+	 * @param {Response} response - The response object to handle.
+	 * @returns {Promise} A promise that resolves to the blob body of the response,
+	 *                    or rejects with an error message.
+	 */
+	static blob(response)
 	{
 		if (!response)
 			return Promise.resolve();
@@ -34,11 +58,18 @@ export default class ResponseHandler
 		if (response.ok)
 			return response.blob();
 
-		return response.text()
-			.then(error => Promise.reject(new Error(error)));
+		return response.text().then((error) => Promise.reject(new Error(error)));
 	}
 
-	static 	none(response)
+	/**
+	 * Returns a promise that resolves with no value if the response is successful,
+	 * or rejects with an error message if the response is not successful.
+	 *
+	 * @param {Response} response - The response object to handle.
+	 * @returns {Promise} A promise that resolves with no value if the response is successful,
+	 *                    or rejects with an error message.
+	 */
+	static none(response)
 	{
 		if (!response)
 			return Promise.resolve();
@@ -46,7 +77,6 @@ export default class ResponseHandler
 		if (response.ok)
 			return Promise.resolve();
 
-		return response.text()
-			.then(error => Promise.reject(new Error(error)));
+		return response.text().then((error) => Promise.reject(new Error(error)));
 	}
 }
