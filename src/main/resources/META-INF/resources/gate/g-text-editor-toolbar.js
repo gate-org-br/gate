8e27b863cf188c54cb0a0fa1d12162e8842eb229
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2023-10-18 04:43:56 -0300

    refactor g-text-editor

---
+++ src/main/resources/META-INF/resources/gate/g-text-editor-toolbar.js
@@ -0,0 +1,222 @@
+let template = document.createElement("template");
+template.innerHTML = `
+	<button tabindex='-1' id='bold' title='Negrito'>
+		&#X3026;
+	</button>
+	<button tabindex='-1' id='italic' title='Itálico'>
+		&#X3027;
+	</button>
+
+	<select id="textDecoration" title='Decoração do texto'>
+		<option value="">Estilo</option>
+		<option value="none">Nenhum</option>
+		<option value="underline">Sublinhado</option>
+		<option value="line-through">Riscado</option>
+		<option value="overline">Sobrelinhado</option>
+	</select>
+
+	<input id='foreColor' type="color" value='#FFFFFF' title='Cor do texto'/>
+
+	<select id="fontSize" title='Tamanho da fonte'>
+		<option value="">Tamanho</option>
+		<option value="1px">1</option>
+		<option value="2px">2</option>
+		<option value="3px">3</option>
+		<option value="4px">4</option>
+		<option value="5px">5</option>
+		<option value="6px">6</option>
+		<option value="7px">7</option>
+		<option value="8px">8</option>
+		<option value="9px">9</option>
+		<option value="10px">10</option>
+		<option value="11px">11</option>
+		<option value="12px">12</option>
+		<option value="13px">13</option>
+		<option value="14px">14</option>
+		<option value="15px">15</option>
+		<option value="16px">16</option>
+		<option value="17px">17</option>
+		<option value="18px">18</option>
+		<option value="19px">19</option>
+		<option value="20px">20</option>
+		<option value="21px">21</option>
+		<option value="22px">22</option>
+		<option value="23px">23</option>
+		<option value="24px">24</option>
+		<option value="25px">25</option>
+		<option value="26px">26</option>
+		<option value="27px">27</option>
+		<option value="28px">28</option>
+		<option value="29px">29</option>
+		<option value="30px">30</option>
+	</select>
+
+	<button tabindex='-1' id='removeFormat' title='Remover formatação'>
+		&#X3030;
+	</button>
+	<span></span>
+	<button tabindex='-1' id='justifyCenter' title='Centralizar'>
+		&#X3034;
+	</button>
+	<button tabindex='-1' id='justifyLeft' title='Alinha à esquerda'>
+		&#X3032;
+	</button>
+	<button tabindex='-1' id='justifyRight' title='Alinha à direita'>
+		&#X3033;
+	</button>
+	<button tabindex='-1' id='justifyFull' title='Justificar'>
+		&#X3031;
+	</button>
+	<span></span>
+	<button tabindex='-1' id='indent' title='Indentar'>
+		&#X3039;
+	</button>
+	<button tabindex='-1' id='outdent' title='Remover indentação'>
+		&#X3040;
+	</button>
+	<span></span>
+	<button tabindex='-1' id='insertUnorderedList' title='Criar lista'>
+		&#X3035;
+	</button>
+	<button tabindex='-1' id='insertOrderedList' title='Criar lista ordenada'>
+		&#X3038;
+	</button>
+	<span></span>
+	<button tabindex='-1' id='createLink' title='Criar link'>
+		&#X2076;
+	</button>
+	<button tabindex='-1' id='unlink' title='Remover link'>
+		&#X2233;
+	</button>
+	<span></span>
+	<button tabindex='-1' id='happyFace' title='Carinha feliz"'>
+		&#X2104;
+	</button>
+	<button tabindex='-1' id='sadFace' title='Carinha triste"'>
+		&#X2106;
+	</button>
+	<button tabindex='-1' id='insertIcon' title='Inserir ícone'>
+		&#X3017;
+	</button>
+	<span></span>
+	<button tabindex='-1' id='attach' title='Anexar arquivo'>
+		&#X2079;
+	</button>
+ <style>:host(*) {
+	gap: 8px;
+	padding: 8px;
+	display: flex;
+	overflow: auto;
+	align-items: center;
+	background-color: #f2f2f2;
+	border-radius: 5px 5px 0 0;
+	justify-content: flex-start;
+	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+}
+
+button {
+	color: #333;
+	height: 40px;
+	min-width: 40px;
+	font-size: 18px;
+	background: #ddd;
+	border: none;
+	border-radius: 5px;
+	font-family: "gate";
+	cursor: pointer; /* Add a pointer cursor on hover */
+	transition: background-color 0.3s; /* Smooth transition for background color */
+}
+
+button:hover {
+	background-color: #ccc;  /* Darker background on hover */
+}
+
+span  {
+	flex: 0 0 8px;
+	display: block;
+}
+
+div {
+	flex-grow: 1;
+	display: block;
+}
+
+#foreColor, #fontSize, #textDecoration
+{
+	width: 80px;
+	height: 40px;
+	border-radius: 5px;
+	border: 1px solid var(--main5);
+}</style>`;
+
+/* global customElements */
+
+customElements.define('g-text-editor-toolbar', class extends HTMLElement
+{
+	constructor()
+	{
+		super();
+		this.tabindex = 1;
+		this.attachShadow({mode: "open"});
+		this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+		let editor = this.editor;
+
+		this.shadowRoot.getElementById("bold").addEventListener("click", () => editor.bold());
+		this.shadowRoot.getElementById("italic").addEventListener("click", () => editor.italic());
+
+		this.shadowRoot.getElementById("textDecoration").addEventListener("change", e => e.target.value && editor.textDecoration(e.target.value) & (e.target.value = ""));
+
+		this.shadowRoot.getElementById("foreColor").addEventListener("change", e => editor.foreColor(e.target.value) & (e.target.value = "#FFFFFF"));
+
+		this.shadowRoot.getElementById("fontSize").addEventListener("change", e => e.target.value && editor.fontSize(e.target.value) & (e.target.value = ""));
+
+		this.shadowRoot.getElementById("removeFormat").addEventListener("click", () => editor.removeFormat());
+
+		this.shadowRoot.getElementById("justifyCenter").addEventListener("click", () => editor.justifyCenter());
+		this.shadowRoot.getElementById("justifyLeft").addEventListener("click", () => editor.justifyLeft());
+		this.shadowRoot.getElementById("justifyRight").addEventListener("click", () => editor.justifyRight());
+		this.shadowRoot.getElementById("justifyFull").addEventListener("click", () => editor.justifyFull());
+
+		this.shadowRoot.getElementById("indent").addEventListener("click", () => editor.indent());
+		this.shadowRoot.getElementById("outdent").addEventListener("click", () => editor.outdent());
+
+		this.shadowRoot.getElementById("insertUnorderedList").addEventListener("click", () => editor.insertUnorderedList());
+		this.shadowRoot.getElementById("insertOrderedList").addEventListener("click", () => editor.insertOrderedList());
+
+		this.shadowRoot.getElementById("createLink").addEventListener("click", () => editor.createLink());
+		this.shadowRoot.getElementById("unlink").addEventListener("click", () => editor.unlink());
+
+		this.shadowRoot.getElementById("happyFace").addEventListener("click", () => editor.happyFace());
+		this.shadowRoot.getElementById("sadFace").addEventListener("click", () => editor.sadFace());
+		this.shadowRoot.getElementById("insertIcon").addEventListener("click", () => editor.insertIcon());
+
+		this.shadowRoot.getElementById("attach").addEventListener("click", () => editor.attach());
+	}
+
+	separator()
+	{
+		this.shadowRoot.appendChild(document.createElement("span"));
+		return this;
+	}
+
+	spacer()
+	{
+		this.shadowRoot.appendChild(document.createElement("div"));
+		return this;
+	}
+
+	command(icon, title, action)
+	{
+		let button = this.appendChild(document.createElement("button"));
+		button.title = title;
+		button.innerHTML = `&#X${icon}`;
+		button.addEventListener("click", action);
+		return this;
+	}
+
+	get editor()
+	{
+		return this.getRootNode().host;
+	}
+});
--- src/main/resources/META-INF/resources/gate/g-text-editor.js
+++ src/main/resources/META-INF/resources/gate/g-text-editor.js
@@ -1,82 +1,7 @@
 let template = document.createElement("template");
 template.innerHTML = `
-	<div id='toolbar'>
-		<button tabindex='-1' id='bold' title='Negrito'>
-			&#X3026;
-		</button>
-		<button tabindex='-1' id='italic' title='Itálico'>
-			&#X3027;
-		</button>
-		<button tabindex='-1' id='underline' title='Sublinhado'>
-			&#X3028;
-		</button>
-		<button tabindex='-1' id='strikeThrough' title='Riscado'>
-			&#X3029;
-		</button>
-		<input id='foreColor' type="color"
-		       value='#FFFFFF' title='Cor do texto'/>
-		<select id="fontSize" title='Tamanho da fonte'>
-			<option value="">0</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-			<option value="7">7</option>
-		</select>
-		<button tabindex='-1' id='removeFormat' title='Remover formatação'>
-			&#X3030;
-		</button>
-		<span></span>
-		<button tabindex='-1' id='justifyCenter' title='Centralizar'>
-			&#X3034;
-		</button>
-		<button tabindex='-1' id='justifyLeft' title='Alinha à esquerda'>
-			&#X3032;
-		</button>
-		<button tabindex='-1' id='justifyRight' title='Alinha à direita'>
-			&#X3033;
-		</button>
-		<button tabindex='-1' id='justifyFull' title='Justificar'>
-			&#X3031;
-		</button>
-		<span></span>
-		<button tabindex='-1' id='indent' title='Indentar'>
-			&#X3039;
-		</button>
-		<button tabindex='-1' id='outdent' title='Remover indentação'>
-			&#X3040;
-		</button>
-		<span></span>
-		<button tabindex='-1' id='insertUnorderedList' title='Criar lista'>
-			&#X3035;
-		</button>
-		<button tabindex='-1' id='insertOrderedList' title='Criar lista ordenada'>
-			&#X3038;
-		</button>
-		<span></span>
-		<button tabindex='-1' id='createLink' title='Criar link'>
-			&#X2076;
-		</button>
-		<button tabindex='-1' id='unlink' title='Remover link'>
-			&#X2233;
-		</button>
-		<span></span>
-		<button tabindex='-1' id='happyFace' title='Carinha feliz"'>
-			&#X2104;
-		</button>
-		<button tabindex='-1' id='sadFace' title='Carinha triste"'>
-			&#X2106;
-		</button>
-		<button tabindex='-1' id='insertIcon' title='Inserir ícone'>
-			&#X3017;
-		</button>
-		<span></span>
-		<button tabindex='-1' id='attach' title='Anexar arquivo'>
-			&#X2079;
-		</button>
-	</div>
+	<g-text-editor-toolbar id='toolbar'>
+	</g-text-editor-toolbar>
 	<div id='scroll'>
 		<div id='editor' tabindex="0" contentEditable='true'>
 		</div>
@@ -92,6 +17,7 @@ template.innerHTML = `
 	align-items: stretch;
 	flex-direction: column;
 	justify-content: stretch;
+	border: 1px solid #f2f2f2;
 }
 
 :host([hidden])
@@ -100,7 +26,7 @@ template.innerHTML = `
 }
 
 #scroll {
-	top: 40px;
+	top: 60px;
 	left: 0;
 	right: 0;
 	bottom: 0;
@@ -109,62 +35,6 @@ template.innerHTML = `
 	position: absolute;
 	align-items: stretch;
 	justify-content: stretch;
-}
-
-#editor picture {
-	padding: 8px;
-	resize: both;
-	display: flex;
-	overflow: auto;
-	max-width: 100%;
-	width: fit-content;
-	height: fit-content;
-	align-items: stretch;
-	justify-content: stretch;
-	border: 1px solid #CCCCCC;
-	background-color: #EFEFEF;
-}
-
-#toolbar
-{
-	gap: 4px;
-	padding: 4px;
-	display: flex;
-	overflow: auto;
-	align-items: center;
-	background-color: var(--main6);
-	border-radius: 5px 5px 0 0;
-	justify-content: flex-start;
-}
-
-button
-{
-	color: black;
-	height: 32px;
-	display: flex;
-	flex: 0 0 32px;
-	font-size: 16px;
-	background: none;
-	border-radius: 5px;
-	align-items: center;
-	font-family: "gate";
-	justify-content: center;
-	border: 1px solid var(--main5);
-	background-color: var(--main4);
-}
-
-button:hover {
-	background-color: var(--main1);
-}
-
-#toolbar > span  {
-	flex: 0 0 8px;
-	display: block;
-}
-
-#toolbar > div {
-	flex-grow: 1;
-	display: block;
 }
 
 #editor
@@ -182,29 +52,18 @@ button:hover {
 	outline: none;
 }
 
-#foreColor, #fontSize
-{
-	width: 64px;
-	height: 32px;
-	border-radius: 5px;
-	border: 1px solid var(--main5);
+i {
+	font-family: gate;
+	font-style: normal;
 }</style>`;
 
 /* global customElements */
 
 import './g-icon.js';
 import './g-text-editor-box.js';
-
-function ensureBoxSpaces(editor)
-{
-	Array.from(editor.querySelectorAll('g-text-editor-box')).forEach(box =>
-	{
-		if (!box.previousElementSibling)
-			box.parentNode.insertBefore(document.createElement("br"), box);
-		if (!box.nextElementSibling)
-			box.parentNode.appendChild(document.createElement("br"));
-	});
-}
+import './g-text-editor-toolbar.js';
+import GIconPicker from './g-icon-picker.js';
+import * as processor from './g-text-processor.js';
 
 customElements.define('g-text-editor', class extends HTMLElement
 {
@@ -214,45 +73,7 @@ customElements.define('g-text-editor', class extends HTMLElement
 		this.tabindex = 0;
 		this.attachShadow({mode: "open"});
 		this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-		this.shadowRoot.getElementById("bold").addEventListener("click", () => this.bold());
-		this.shadowRoot.getElementById("italic").addEventListener("click", () => this.italic());
-		this.shadowRoot.getElementById("underline").addEventListener("click", () => this.underline());
-		this.shadowRoot.getElementById("strikeThrough").addEventListener("click", () => this.strikeThrough());
-
-		this.shadowRoot.getElementById("foreColor").addEventListener("change",
-			e => this.foreColor(e.target.value) & (e.target.value = "#FFFFFF"));
-
-		this.shadowRoot.getElementById("fontSize")
-			.addEventListener("change", e => e.target.value
-					&& this.fontSize(e.target.value)
-					& (e.target.value = ""));
-
-		this.shadowRoot.getElementById("removeFormat").addEventListener("click", () => this.removeFormat());
-
-		this.shadowRoot.getElementById("justifyCenter").addEventListener("click", () => this.justifyCenter());
-		this.shadowRoot.getElementById("justifyLeft").addEventListener("click", () => this.justifyLeft());
-		this.shadowRoot.getElementById("justifyRight").addEventListener("click", () => this.justifyRight());
-		this.shadowRoot.getElementById("justifyFull").addEventListener("click", () => this.justifyFull());
-
-		this.shadowRoot.getElementById("indent").addEventListener("click", () => this.indent());
-		this.shadowRoot.getElementById("outdent").addEventListener("click", () => this.outdent());
-
-		this.shadowRoot.getElementById("insertUnorderedList").addEventListener("click", () => this.insertUnorderedList());
-		this.shadowRoot.getElementById("insertOrderedList").addEventListener("click", () => this.insertOrderedList());
-
-		this.shadowRoot.getElementById("createLink").addEventListener("click", () => this.createLink());
-		this.shadowRoot.getElementById("unlink").addEventListener("click", () => this.unlink());
-
-		this.shadowRoot.getElementById("happyFace").addEventListener("click", () => this.happyFace());
-		this.shadowRoot.getElementById("sadFace").addEventListener("click", () => this.sadFace());
-		this.shadowRoot.getElementById("insertIcon").addEventListener("click", () => this.insertIcon());
-
-		this.shadowRoot.getElementById("attach").addEventListener("click", () => this.attach());
-
 		let editor = this.shadowRoot.getElementById("editor");
-		editor.addEventListener('input', event => ensureBoxSpaces(editor));
-		editor.addEventListener('click', event => ensureBoxSpaces(editor));
 
 		this.addEventListener("focus", () =>
 		{
@@ -268,69 +89,77 @@ customElements.define('g-text-editor', class extends HTMLElement
 
 	bold()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("bold", false, null);
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.toggle(editor, "font-weight", "700", "400");
 	}
 
 	italic()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("italic");
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.toggle(editor, "font-style", "italic", "normal");
 	}
 
-	underline()
+	textDecoration(value)
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("underline");
-	}
-
-	strikeThrough()
-	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("strikeThrough");
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.apply(editor, "text-decoration-line", value);
 	}
 
 	fontSize(value)
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("fontSize", null, value);
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.apply(editor, "font-size", value);
 	}
 
 	foreColor(value)
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("foreColor", null, value);
-
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.apply(editor, "color", value);
 	}
 
 	removeFormat()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("removeFormat");
+
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.removeSytles(editor);
 	}
 
 	justifyCenter()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("justifyCenter");
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.apply(editor, "display", "block");
+		processor.apply(editor, "text-align", "center");
 	}
 
 	justifyLeft()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("justifyLeft");
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.apply(editor, "display", "block");
+		processor.apply(editor, "text-align", "left");
 	}
 
 	justifyRight()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("justifyRight");
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.apply(editor, "display", "block");
+		processor.apply(editor, "text-align", "right");
 	}
 
 	justifyFull()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("justifyFull");
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.apply(editor, "display", "block");
+		processor.apply(editor, "text-align", "justify");
 	}
 
 	indent()
@@ -371,14 +200,23 @@ customElements.define('g-text-editor', class extends HTMLElement
 
 	happyFace()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("insertHTML", null, `<i>&#X2104</i>`);
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.insertIcon(editor, "2104");
 	}
 
 	sadFace()
 	{
-		this.shadowRoot.getElementById("editor").focus();
-		document.execCommand("insertHTML", null, `<i>&#X2106</i>`);
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		processor.insertIcon(editor, "2106");
+	}
+
+	insertIcon()
+	{
+		let editor = this.shadowRoot.getElementById("editor");
+		editor.focus();
+		GIconPicker.pick().then(e => e && processor.insertIcon(editor, e));
 	}
 
 	set hidden(value)
@@ -392,14 +230,6 @@ customElements.define('g-text-editor', class extends HTMLElement
 	get hidden()
 	{
 		return this.hasAtribute("hidden");
-	}
-
-	insertIcon()
-	{
-		this.shadowRoot.getElementById("editor").focus();
-		let picker = window.top.document.createElement("g-icon-picker");
-		picker.addEventListener("picked", e => document.execCommand("insertHTML", null, `<g-icon>&#X${e.detail}</g-icon>`));
-		picker.show();
 	}
 
 	attach()
@@ -535,28 +365,9 @@ customElements.define('g-text-editor', class extends HTMLElement
 		this.setAttribute("tabindex", tabindex);
 	}
 
-	separator()
+	get toolbar()
 	{
-		this.shadowRoot.getElementById("toolbar")
-			.appendChild(document.createElement("span"));
-		return this;
-	}
-
-	spacer()
-	{
-		this.shadowRoot.getElementById("toolbar")
-			.appendChild(document.createElement("div"));
-		return this;
-	}
-
-	command(icon, title, action)
-	{
-		let button = this.shadowRoot.getElementById("toolbar")
-			.appendChild(document.createElement("button"));
-		button.title = title;
-		button.innerHTML = `&#X${icon}`;
-		button.addEventListener("click", action);
-		return this;
+		return this.shadowRoot.getElementById("toolbar");
 	}
 
 	attributeChangedCallback()
