c1843b098191ce259ff7081a1b5720027c660a81
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2024-07-07 19:33:56 -0300

    Fix error with file uploads with progress

--- src/main/wc/process.mjs
+++ src/main/wc/process.mjs
@@ -1,65 +1,75 @@
 import SSE from './sse.js';
 
-export default function process(id, name, method, action, body)
+function parseEvent(string)
+{
+	let event = JSON.parse(String(atob(string)));
+	event.toString = function ()
+	{
+		if (this.done && this.done !== -1)
+			if (this.todo && this.todo !== -1)
+				return this.done + "/" + this.todo;
+			else
+				return this.done.toString();
+		else
+			return "...";
+	};
+	return event;
+}
+
+export default function process(id, name, method, action, payload)
 {
 	return new Promise((resolve, reject) =>
 	{
 		window.top.dispatchEvent(new CustomEvent('ProcessRequest',
 			{detail: {id, name}}));
 
-		if (body instanceof HTMLFormElement)
-			body = new FormData(body);
+		method = (method || "GET").toUpperCase();
 
-		let source = new SSE(action, {start: false, method, body});
-		source.addEventListener("message", (event) =>
+		if (payload instanceof HTMLFormElement)
+			payload = new FormData(payload);
+
+		let source = new SSE(action, {start: false, method, payload});
+
+		source.addEventListener("Progress", (event) =>
 		{
-			event = JSON.parse(String(atob(event.data)));
-			event.toString = function ()
+			event = parseEvent(event.data);
+
+			switch (event.status)
 			{
-				if (this.done && this.done !== -1)
-					if (this.todo && this.todo !== -1)
-						return this.done + "/" + this.todo;
-					else
-						return this.done.toString();
-				else
-					return "...";
-			};
-			switch (event.event)
-			{
-				case "Progress":
-					switch (event.status)
-					{
-						case "CREATED":
-							window.top.dispatchEvent(new CustomEvent('ProcessPending',
-								{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
-							break;
-						case "PENDING":
-							window.top.dispatchEvent(new CustomEvent('ProcessPending',
-								{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
-							break;
-						case "COMMITED":
-							window.top.dispatchEvent(new CustomEvent('ProcessCommited',
-								{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
-							break;
-						case "CANCELED":
-							window.top.dispatchEvent(new CustomEvent('ProcessCanceled',
-								{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
-							break;
-					}
+				case "CREATED":
+					window.top.dispatchEvent(new CustomEvent('ProcessPending',
+						{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
 					break;
-				case "Result":
-					const {contentType = 'text/plain;charset=utf-8', filename, data} = event;
-					window.top.dispatchEvent(new CustomEvent('ProccessResult', {id, name, contentType, filename, data}));
-
-					const headers = new Headers();
-					headers.append("Content-Type", contentType);
-					if (filename)
-						headers.append("Content-Disposition",
-							`attachment; filename="${filename}"`);
-
-					resolve(new Response(data, {status: 200, statusText: 'OK', headers}));
+				case "PENDING":
+					window.top.dispatchEvent(new CustomEvent('ProcessPending',
+						{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
+					break;
+				case "COMMITED":
+					window.top.dispatchEvent(new CustomEvent('ProcessCommited',
+						{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
+					break;
+				case "CANCELED":
+					window.top.dispatchEvent(new CustomEvent('ProcessCanceled',
+						{detail: {id, name, todo: event.todo, done: event.done, text: event.text, progress: event.toString()}}));
 					break;
 			}
+		});
+
+
+		source.addEventListener("Result", (event) =>
+		{
+			event = parseEvent(event.data);
+
+			const {contentType = 'text/plain;charset=utf-8', filename, data} = event;
+			window.top.dispatchEvent(new CustomEvent('ProccessResult', {id, name, contentType, filename, data}));
+
+			const headers = new Headers();
+			headers.append("Content-Type", contentType);
+			if (filename)
+				headers.append("Content-Disposition",
+					`attachment; filename="${filename}"`);
+
+			resolve(new Response(data, {status: 200, statusText: 'OK', headers}));
 		});
 
 		source.addEventListener("error", e =>
@@ -67,6 +77,19 @@ export default function process(id, name, method, action, body)
 			window.top.dispatchEvent(new CustomEvent('ProcessError',
 				{detail: {id, name, text: "Conexão perdida com o servidor"}}));
 			reject(e.text);
+		});
+
+		source.addEventListener("abort", e =>
+		{
+			window.top.dispatchEvent(new CustomEvent('ProcessError',
+				{detail: {id, name, text: "Conexão perdida com o servidor"}}));
+			reject("Connection closed");
+		});
+
+		source.addEventListener("readystatechange", e =>
+		{
+			if (e.readyState === 2)
+				resolve(null);
 		});
 
 		source.stream();
