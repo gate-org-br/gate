c58edf601a8eafda362f9a997c95d6972b65dac0
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2024-11-20 19:41:22 -0300

    Fix error with user producer

--- src/main/java/gate/producer/UserProducer.java
+++ src/main/java/gate/producer/UserProducer.java
@@ -3,22 +3,22 @@ package gate.producer;
 import gate.annotation.Current;
 import gate.entity.User;
 import gate.error.InvalidCredentialsException;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+@ApplicationScoped
 public class UserProducer
 {
 
-	@Inject
-	private HttpServletRequest request;
-
 	@Current
 	@Produces
+	@RequestScoped
 	@Named(value = "user")
-	public User getUser() throws InvalidCredentialsException
+	public User getUser(HttpServletRequest request) throws InvalidCredentialsException
 	{
 		User user = (User) request.getAttribute(User.class.getName());
 		if (user != null)
