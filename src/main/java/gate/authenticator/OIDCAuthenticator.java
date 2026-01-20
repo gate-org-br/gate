0c506558c213b6bcd94318c517c93f00ba899d4f
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2026-01-20 19:52:49 -0300

    improve oidc default redirect url

--- src/main/java/gate/authenticator/OIDCAuthenticator.java
+++ src/main/java/gate/authenticator/OIDCAuthenticator.java
@@ -68,7 +68,7 @@ public class OIDCAuthenticator implements Authenticator
 
 		redirectUri = config.getProperty("oidc.redirect_uri")
 			.orElse("default".equals(config.name())
-				? "${server}"
+				? "${server}/Gate"
 				: "${server}/Gate?authenticator=" + config.name());
 
 		userId = config.getProperty("oidc.user_id").orElse("email");
