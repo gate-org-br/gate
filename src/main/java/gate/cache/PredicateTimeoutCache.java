cec1c6ffd56b3027a2c2aad3a881187c902b99cf
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2026-01-21 21:32:56 -0300

    fix predicate cache

--- src/main/java/gate/cache/PredicateTimeoutCache.java
+++ src/main/java/gate/cache/PredicateTimeoutCache.java
@@ -5,45 +5,53 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-class PredicateTimeoutCache<T> implements Cache<T> {
+class PredicateTimeoutCache<T> implements Cache<T>
+{
 
 	private final Duration timeout;
 	private final Supplier<T> supplier;
 	private final Predicate<T> predicate;
 	private final AtomicReference<CacheEntry<T>> value = new AtomicReference<>();
 
-	PredicateTimeoutCache(Duration timeout, Predicate<T> predicate, Supplier<T> supplier) {
+	PredicateTimeoutCache(Duration timeout, Predicate<T> predicate, Supplier<T> supplier)
+	{
 		this.timeout = timeout;
 		this.predicate = predicate;
 		this.supplier = supplier;
 	}
 
 	@Override
-	public T get() {
+	public T get()
+	{
 		return value.updateAndGet(e -> e != null && System.currentTimeMillis() - e.getTimestamp() < timeout.toMillis()
-				&& !predicate.test(e.getValue()) ? e : new CacheEntry<T>(supplier.get())).getValue();
+			&& predicate.test(e.getValue()) ? e : new CacheEntry<T>(supplier.get())).getValue();
 	}
 
 	@Override
-	public void invalidate() {
+	public void invalidate()
+	{
 		value.set(null);
 	}
 
-	private static class CacheEntry<T> {
+	private static class CacheEntry<T>
+	{
 
 		private final T value;
 		private final long timestamp;
 
-		private CacheEntry(T value) {
+		private CacheEntry(T value)
+		{
 			this.value = value;
 			this.timestamp = System.currentTimeMillis();
 		}
 
-		T getValue() {
+		T getValue()
+		{
 			return value;
 		}
 
-		long getTimestamp() {
+		long getTimestamp()
+		{
 			return timestamp;
 		}
 	}
