b61d055a1e306e85d92f483405c2c7ab657fa48d
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2026-02-09 21:03:13 -0300

    Improve SSE events

--- src/main/java/gate/Gate.java
+++ src/main/java/gate/Gate.java
@@ -23,7 +23,6 @@ import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.spi.CDI;
 import jakarta.inject.Inject;
 import jakarta.servlet.AsyncContext;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -44,222 +43,227 @@ import java.util.Locale;
 public class Gate extends HttpServlet
 {
 
-	public static final String SUBJECT_COOKIE = "subject";
-	static final String HTML = "/views/Gate.html";
+    public static final String SUBJECT_COOKIE = "subject";
+    static final String HTML = "/views/Gate.html";
 
-	@Inject
-	Logger logger;
+    @Inject
+    Logger logger;
 
-	@Inject
-	Event<AppEvent> event;
+    @Inject
+    Event<AppEvent> event;
 
-	@Any
-	@Inject
-	Instance<Handler> handlers;
+    @Any
+    @Inject
+    Instance<Handler> handlers;
 
-	@Any
-	@Inject
-	Instance<Catcher> catchers;
+    @Any
+    @Inject
+    Instance<Catcher> catchers;
 
-	@Inject
-	@Current
-	@RequestScoped
-	Authenticator authenticator;
+    @Inject
+    @Current
+    @RequestScoped
+    Authenticator authenticator;
 
-	@Inject
-	Call mainAction;
+    @Inject
+    Call mainAction;
 
-	@Inject
-	ThreadContext threadContext;
+    @Inject
+    @SuppressWarnings("CdiInjectionPointsInspection")
+    ThreadContext threadContext;
 
-	@Inject
-	Credentials credentials;
+    @Inject
+    Credentials credentials;
 
-	@Inject
-	@Current
-	@RequestScoped
-	Instance<User> userInstance;
+    @Inject
+    @Current
+    @RequestScoped
+    Instance<User> userInstance;
 
-	static
-	{
-		Locale.setDefault(new Locale("pt", "BR"));
-	}
+    @Inject
+    @SuppressWarnings("unused")
+    Heartbeat heartbeat;
 
-	@Override
-	public void service(HttpServletRequest httpServletRequest, HttpServletResponse response)
-		throws ServletException, IOException
-	{
-		ScreenServletRequest request = new ScreenServletRequest(httpServletRequest);
+    static
+    {
+        Locale.setDefault(new Locale("pt", "BR"));
+    }
 
-		try
-		{
-			User user = userInstance.get();
+    @Override
+    public void service(HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException
+    {
+        ScreenServletRequest request = new ScreenServletRequest(httpServletRequest);
 
-			httpServletRequest.setCharacterEncoding("UTF-8");
-			response.setCharacterEncoding("UTF-8");
-			response.setLocale(Locale.getDefault());
+        try
+        {
+            User user = userInstance.get();
 
-			String MODULE = request.getParameter("MODULE");
-			String SCREEN = request.getParameter("SCREEN");
-			String ACTION = request.getParameter("ACTION");
-			if (Toolkit.isEmpty(MODULE, SCREEN, ACTION) && httpServletRequest.getPathInfo() != null)
-			{
-				List<String> path = Toolkit.parsePath(httpServletRequest.getPathInfo());
-				MODULE = !path.isEmpty() ? path.get(0) : null;
-				SCREEN = path.size() >= 2 ? path.get(1) : null;
-				ACTION = path.size() >= 3 ? path.get(2) : null;
-			}
+            httpServletRequest.setCharacterEncoding("UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            response.setLocale(Locale.getDefault());
 
-			request.setAttribute("ACTION", ACTION);
-			request.setAttribute("MODULE", MODULE);
-			request.setAttribute("SCREEN", SCREEN);
+            String MODULE = request.getParameter("MODULE");
+            String SCREEN = request.getParameter("SCREEN");
+            String ACTION = request.getParameter("ACTION");
+            if (Toolkit.isEmpty(MODULE, SCREEN, ACTION) && httpServletRequest.getPathInfo() != null)
+            {
+                List<String> path = Toolkit.parsePath(httpServletRequest.getPathInfo());
+                MODULE = !path.isEmpty() ? path.get(0) : null;
+                SCREEN = path.size() >= 2 ? path.get(1) : null;
+                ACTION = path.size() >= 3 ? path.get(2) : null;
+            }
 
-			if (Toolkit.isEmpty(MODULE, SCREEN, ACTION)
-				&& !authenticator.hasCredentials(request))
-			{
-				String provider = authenticator.provider(request, response);
-				if (provider != null)
-					response.sendRedirect(provider);
-				else
-					handlers.select(HTMLCommandHandler.class).get().handle(httpServletRequest, response, HTML);
-				return;
-			}
+            request.setAttribute("ACTION", ACTION);
+            request.setAttribute("MODULE", MODULE);
+            request.setAttribute("SCREEN", SCREEN);
 
-			if (authenticator.hasCredentials(request))
-			{
-				user = authenticator.authenticate(request, response);
-				if (user != null)
-				{
-					event.fireAsync(new LoginEvent(user));
-					response.addCookie(CookieFactory.create(SUBJECT_COOKIE, credentials.subject(user)));
-				}
-				request.setAttribute(User.class.getName(), user);
+            if (Toolkit.isEmpty(MODULE, SCREEN, ACTION)
+                    && !authenticator.hasCredentials(request))
+            {
+                String provider = authenticator.provider(request, response);
+                if (provider != null)
+                    response.sendRedirect(provider);
+                else
+                    handlers.select(HTMLCommandHandler.class).get().handle(httpServletRequest, response, HTML);
+                return;
+            }
 
-				if (Toolkit.isEmpty(MODULE, SCREEN, ACTION))
-				{
-					if (mainAction == Call.NONE)
-						throw new InternalServerException("No main action defined");
-					response.sendRedirect(mainAction.toString());
-					return;
-				}
-			}
+            if (authenticator.hasCredentials(request))
+            {
+                user = authenticator.authenticate(request, response);
+                if (user != null)
+                {
+                    event.fireAsync(new LoginEvent(user));
+                    response.addCookie(CookieFactory.create(SUBJECT_COOKIE, credentials.subject(user)));
+                }
+                request.setAttribute(User.class.getName(), user);
 
-			Call call = Call.of(MODULE, SCREEN, ACTION);
-			if (!call.checkAccess(user))
-				if (user != null && user.getId() != null)
-					throw new ForbiddenException();
-				else
-					throw new UnauthorizedException();
+                if (Toolkit.isEmpty(MODULE, SCREEN, ACTION))
+                {
+                    if (mainAction == Call.NONE)
+                        throw new InternalServerException("No main action defined");
+                    response.sendRedirect(mainAction.toString());
+                    return;
+                }
+            }
 
-			Screen screen = CDI.current().select(call.getType()).get();
-			request.setAttribute("screen", screen);
-			request.setAttribute("action", call.getMethod());
-			screen.prepare(request, response);
+            Call call = Call.of(MODULE, SCREEN, ACTION);
+            if (!call.checkAccess(user))
+                if (user != null && user.getId() != null)
+                    throw new ForbiddenException();
+                else
+                    throw new UnauthorizedException();
 
-			if (call.getMethod().isAnnotationPresent(Cors.class))
-			{
-				response.setHeader("Access-Control-Max-Age", "3600");
-				response.setHeader("Access-Control-Allow-Credentials", "true");
-				response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-				response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-				response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-			}
+            Screen screen = CDI.current().select(call.getType()).get();
+            request.setAttribute("screen", screen);
+            request.setAttribute("action", call.getMethod());
+            screen.prepare(request, response);
 
-			if (call.getMethod().isAnnotationPresent(Asynchronous.class))
-				executeAsync(user, request, response, screen, call.getMethod());
-			else
-				execute(httpServletRequest, response, screen, call.getMethod());
+            if (call.getMethod().isAnnotationPresent(Cors.class))
+            {
+                response.setHeader("Access-Control-Max-Age", "3600");
+                response.setHeader("Access-Control-Allow-Credentials", "true");
+                response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+                response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+                response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+            }
 
-		} catch (AuthenticationException ex)
-		{
-			if (authenticator.provider(request, response) != null)
-			{
-				var type = Catcher.getCatcher(ex.getClass());
-				Catcher catcher = catchers.select(type).get();
-				catcher.catches(httpServletRequest, response, ex);
-			} else
-			{
-				httpServletRequest.setAttribute("messages", Collections.singletonList(ex.getMessage()));
-				httpServletRequest.setAttribute("exception", ex);
-				Handler handler = handlers.select(HTMLCommandHandler.class).get();
-				handler.handle(httpServletRequest, response, HTML);
-			}
-		} catch (RuntimeException ex)
-		{
-			var type = Catcher.getCatcher(ex.getClass());
-			Catcher catcher = catchers.select(type).get();
-			catcher.catches(httpServletRequest, response, ex);
-		}
-	}
+            if (call.getMethod().isAnnotationPresent(Asynchronous.class))
+                executeAsync(user, request, response, screen, call.getMethod());
+            else
+                execute(httpServletRequest, response, screen, call.getMethod());
 
-	private void execute(HttpServletRequest request, HttpServletResponse response, Screen screen,
-		Method method)
-	{
-		try
-		{
-			Object result = screen.execute(method);
-			if (result != null)
-			{
-				var type = method.isAnnotationPresent(gate.annotation.Handler.class)
-					? method.getAnnotation(gate.annotation.Handler.class).value()
-					: Handler.getHandler(result.getClass());
-				var handler = handlers.select(type).get();
-				handler.handle(request, response, result);
-			}
-		} catch (Throwable ex)
-		{
-			var type = Catcher.getCatcher(ex.getClass());
-			Catcher catcher = catchers.select(type).get();
-			catcher.catches(request, response, ex);
-		}
-	}
+        } catch (AuthenticationException ex)
+        {
+            if (authenticator.provider(request, response) != null)
+            {
+                var type = Catcher.getCatcher(ex.getClass());
+                Catcher catcher = catchers.select(type).get();
+                catcher.catches(httpServletRequest, response, ex);
+            } else
+            {
+                httpServletRequest.setAttribute("messages", Collections.singletonList(ex.getMessage()));
+                httpServletRequest.setAttribute("exception", ex);
+                Handler handler = handlers.select(HTMLCommandHandler.class).get();
+                handler.handle(httpServletRequest, response, HTML);
+            }
+        } catch (RuntimeException ex)
+        {
+            var type = Catcher.getCatcher(ex.getClass());
+            Catcher catcher = catchers.select(type).get();
+            catcher.catches(httpServletRequest, response, ex);
+        }
+    }
 
-	private void executeAsync(User user, ScreenServletRequest request, HttpServletResponse response,
-		Screen screen, Method method)
-	{
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/event-stream");
-		response.setHeader("Cache-Control", "no-cache");
-		response.setHeader("Connection", "keep-alive");
+    private void execute(HttpServletRequest request, HttpServletResponse response, Screen screen,
+                         Method method)
+    {
+        try
+        {
+            Object result = screen.execute(method);
+            if (result != null)
+            {
+                var type = method.isAnnotationPresent(gate.annotation.Handler.class)
+                        ? method.getAnnotation(gate.annotation.Handler.class).value()
+                        : Handler.getHandler(result.getClass());
+                var handler = handlers.select(type).get();
+                handler.handle(request, response, result);
+            }
+        } catch (Throwable ex)
+        {
+            var type = Catcher.getCatcher(ex.getClass());
+            Catcher catcher = catchers.select(type).get();
+            catcher.catches(request, response, ex);
+        }
+    }
 
-		AsyncContext asyncContext = request.startAsync(request, response);
-		asyncContext.setTimeout(0);
+    private void executeAsync(User user, ScreenServletRequest request, HttpServletResponse response,
+                              Screen screen, Method method)
+    {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/event-stream");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Connection", "keep-alive");
 
-		Runnable contextualTask = threadContext.contextualRunnable(() ->
-		{
-			try (Writer writer = response.getWriter())
-			{
-				Progress progress = Progress.create(user, writer);
-				try
-				{
-					Object result = screen.execute(method);
-					if (result != null)
-					{
-						var type = method.isAnnotationPresent(gate.annotation.Handler.class)
-							? method.getAnnotation(gate.annotation.Handler.class).value()
-							: Handler.getHandler(result.getClass());
-						var handler = handlers.select(type).get();
-						handler.handle(request, response, progress, result);
-					}
-					progress.close();
-				} catch (AppException ex)
-				{
-					progress.abort(ex.getMessage());
-				} catch (Throwable ex)
-				{
-					progress.abort(ex.getMessage());
-					logger.error(ex.getMessage(), ex);
-				}
-			} catch (IOException ex)
-			{
-				logger.error(ex.getMessage(), ex);
-			} finally
-			{
-				TempFile.cleanup();
-				asyncContext.complete();
-			}
-		});
+        AsyncContext asyncContext = request.startAsync(request, response);
+        asyncContext.setTimeout(0);
 
-		asyncContext.start(contextualTask);
-	}
+        Runnable contextualTask = threadContext.contextualRunnable(() ->
+        {
+            try (Writer writer = response.getWriter())
+            {
+                Progress progress = Progress.create(writer);
+                try
+                {
+                    Object result = screen.execute(method);
+                    if (result != null)
+                    {
+                        var type = method.isAnnotationPresent(gate.annotation.Handler.class)
+                                ? method.getAnnotation(gate.annotation.Handler.class).value()
+                                : Handler.getHandler(result.getClass());
+                        var handler = handlers.select(type).get();
+                        handler.handle(request, response, progress, result);
+                    }
+                    progress.close();
+                } catch (AppException ex)
+                {
+                    progress.abort(ex.getMessage());
+                } catch (Throwable ex)
+                {
+                    progress.abort(ex.getMessage());
+                    logger.error(ex.getMessage(), ex);
+                }
+            } catch (IOException ex)
+            {
+                logger.error(ex.getMessage(), ex);
+            } finally
+            {
+                Progress.finish();
+                TempFile.cleanup();
+                asyncContext.complete();
+            }
+        });
+
+        asyncContext.start(contextualTask);
+    }
 }
--- src/main/java/gate/SSEServlet.java
+++ src/main/java/gate/SSEServlet.java
@@ -4,147 +4,85 @@ import gate.annotation.Current;
 import gate.catcher.UnauthorizedExceptionCatcher;
 import gate.entity.User;
 import gate.error.UnauthorizedException;
-import gate.event.AppEvent;
 import gate.http.ScreenServletRequest;
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.enterprise.event.Observes;
-import jakarta.enterprise.event.ObservesAsync;
+import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
-import jakarta.servlet.*;
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.AsyncEvent;
+import jakarta.servlet.AsyncListener;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 @WebServlet(value = "/SSE", asyncSupported = true)
 public class SSEServlet extends HttpServlet
 {
-	@Inject
-	UnauthorizedExceptionCatcher catcher;
+    @Inject
+    UnauthorizedExceptionCatcher catcher;
 
-	@Inject
-	@Current
-	@RequestScoped
-	User user;
+    @Inject
+    @Current
+    @RequestScoped
+    Instance<User> userInstance;
 
-	private static final List<Client> clients = new CopyOnWriteArrayList<>();
+    @Inject
+    SSEClients clients;
 
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.addHeader("Cache-Control", "no-cache");
+        ScreenServletRequest request = new ScreenServletRequest(httpServletRequest);
 
-	@Override
-	protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse response) throws ServletException, IOException
-	{
-		ScreenServletRequest request = new ScreenServletRequest(httpServletRequest);
+        try
+        {
+            User user = userInstance.get();
+            if (user == null || user.getId() == null)
+                throw new UnauthorizedException();
 
-		try
-		{
-			if (user == null || user.getId() == null)
-				throw new UnauthorizedException();
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/event-stream");
 
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("text/event-stream");
+            AsyncContext context = request.startAsync();
+            context.setTimeout(TimeUnit.HOURS.toMillis(1));
 
-			AsyncContext context = request.startAsync();
-			context.setTimeout(TimeUnit.HOURS.toMillis(1));
+            SSEClient client = new SSEClient(user.unwrap(), context);
+            clients.add(client);
 
-			Client client = new Client(user.unwrap(), context);
-			clients.add(client);
+            context.addListener(new AsyncListener()
+            {
+                @Override
+                public void onStartAsync(AsyncEvent event)
+                {
+                }
 
-			context.addListener(new AsyncListener()
-			{
-				@Override
-				public void onStartAsync(AsyncEvent event)
-				{
-				}
+                @Override
+                public void onComplete(AsyncEvent event)
+                {
+                    clients.remove(client);
+                }
 
-				@Override
-				public void onComplete(AsyncEvent event)
-				{
-					client.close();
-					clients.remove(client);
-				}
+                @Override
+                public void onError(AsyncEvent event)
+                {
+                    onComplete(event);
+                }
 
-				@Override
-				public void onError(AsyncEvent event)
-				{
-					client.close();
-					clients.remove(client);
-				}
-
-				@Override
-				public void onTimeout(AsyncEvent event)
-				{
-					client.close();
-					clients.remove(client);
-				}
-			});
-		} catch (UnauthorizedException ex)
-		{
-			catcher.catches(request, response, ex);
-		}
-	}
-
-	public void onAPPEvent(@Observes AppEvent event)
-	{
-		clients.forEach(e -> e.send(event));
-	}
-
-	public void onAPPEventAsync(@ObservesAsync AppEvent event)
-	{
-		clients.forEach(e -> e.send(event));
-	}
-
-	private static class Client
-	{
-		private final User subject;
-		private ServletOutputStream out;
-		private final AsyncContext asyncContext;
-
-		public Client(User user, AsyncContext asyncContext)
-		{
-			this.subject = user;
-			this.asyncContext = asyncContext;
-		}
-
-		public synchronized void send(AppEvent event)
-		{
-			if (event.checkAccess(subject))
-			{
-				try
-				{
-					if (out == null)
-						out = asyncContext.getResponse().getOutputStream();
-
-					var data = Base64.getEncoder()
-							.encodeToString(event.toString()
-									.getBytes(StandardCharsets.UTF_8));
-
-					out.println("event: message");
-					out.println("data: " + data);
-					out.println();
-					out.flush();
-					asyncContext.getResponse().flushBuffer();
-				} catch (IOException | RuntimeException ex)
-				{
-					asyncContext.complete();
-				}
-			}
-		}
-
-		public void close()
-		{
-			try
-			{
-				out.close();
-			} catch (Exception ignored)
-			{
-			}
-		}
-	}
+                @Override
+                public void onTimeout(AsyncEvent event)
+                {
+                    onComplete(event);
+                }
+            });
+        } catch (UnauthorizedException ex)
+        {
+            catcher.catches(request, response, ex);
+        }
+    }
 }
