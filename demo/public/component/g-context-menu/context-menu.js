8c17bf45e39a005718d16ce408a70e9a12ce06d8
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2026-02-21 12:25:05 -0300

    Rename demo folder to documentation

--- demo/public/component/g-context-menu/context-menu.js
+++
@@ -1,8 +0,0 @@
-export default [{"text": "function", "icon": "2008", "action": () => alert("Item 1")},
-	{"text": "_blank", "icon": "2009", "action": "http://www.google.com", "target": "_blank"},
-	{"text": "@dialog", "icon": "2006", "action": "message.txt", "target": "@dialog", "title": "Dialog"},
-	{"text": "Submenu", "icon": "2207",
-		"action": [{"text": "Action", "icon": "2008", "action": () => alert("Item 1")},
-			{"text": "_blank", "icon": "2009", "action": "http://www.google.com", "target": "_blank"},
-			{"text": "@dialog", "icon": "2006", "action": "http://www.google.com", "target": "@dialog"}]
-	}]
