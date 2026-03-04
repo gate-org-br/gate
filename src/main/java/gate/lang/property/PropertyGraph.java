30c96127a2c22cde0e68d94d70c4a19ec9342e90
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2026-03-03 23:26:05 -0300

    Implement ddd support on property graph

--- src/main/java/gate/lang/property/PropertyGraph.java
+++ src/main/java/gate/lang/property/PropertyGraph.java
@@ -1,12 +1,11 @@
 package gate.lang.property;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-
 import gate.error.ConversionException;
+import gate.util.Reflection;
+
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.function.Function;
 
 public class PropertyGraph<T>
 {
@@ -46,53 +45,120 @@ public class PropertyGraph<T>
 
 	public Object get(Object value, Function<Property, Object> getValue) throws ConversionException
 	{
+		return get(type, value, graph, getValue);
+	}
+
+	private Object get(Class<?> type, Object value, Object properties, Function<Property, Object> getValue)
+	{
 		try
 		{
-			return get(type, value, graph, getValue);
+			if (properties instanceof Property property)
+				return getValue.apply(property);
+
+			if (!(properties instanceof Map<?, ?> map))
+				return null;
+
+			@SuppressWarnings("unchecked")
+			var propertyMap = (Map<Attribute, Object>) map;
+
+			if (type.isRecord())
+				return fromRecord(type, propertyMap, getValue);
+
+			var builderFactory = Reflection.findMethod(type, "builder").orElse(null);
+			if (builderFactory != null)
+				return fromBuilder(builderFactory, value, propertyMap, getValue);
+
+			if (type.isInterface()
+					|| Arrays.stream(type.getDeclaredConstructors())
+					.anyMatch(c -> c.getParameterCount() == 0))
+				return fromAnemic(type, value, propertyMap, getValue);
+
+			return fromCanonicalConstructor(type, propertyMap, getValue);
 		} catch (ReflectiveOperationException ex)
 		{
 			throw new ConversionException("Error trying to match", ex);
 		}
 	}
 
-	private Object get(Class<?> type, Object value, Object properties, Function<Property, Object> getValue)
+	private Object fromRecord(Class<?> type, Map<Attribute, Object> propertyMap, Function<Property, Object> getValue)
 			throws ReflectiveOperationException
 	{
-		if (properties instanceof Property property)
-			return getValue.apply(property);
-
-		if (properties instanceof Map<?, ?> map)
+		var values = new ArrayList<>();
+		var types = new ArrayList<Class<?>>();
+		for (var component : type.getRecordComponents())
 		{
-			if (type.isRecord())
-			{
-				var types = new ArrayList<Class<?>>();
-				List<Object> values = new ArrayList<>();
-				for (var component : type.getRecordComponents())
-				{
-					types.add(component.getType());
-					var parameters = map.entrySet().stream()
-							.filter(e -> e.getKey().toString().equals(component.getName())).map(Map.Entry::getValue)
-							.findAny().orElse(null);
-					values.add(get(component.getType(), null, parameters, getValue));
-				}
-				var constructor = type.getDeclaredConstructor(types.toArray(new Class[0]));
-				constructor.setAccessible(true);
-				return constructor.newInstance(values.toArray());
-			} else
-			{
-				if (value == null)
-					value = ObjectFactory.create(type);
-				for (var entry : map.entrySet())
-				{
-					var attribute = ((Attribute) entry.getKey());
-					var currentValue = attribute.getValue(value);
-					var newValue = get(attribute.getRawType(), currentValue, entry.getValue(), getValue);
-					if (newValue != currentValue)
-						attribute.setValue(value, newValue);
-				}
-				return value;
-			}
+			types.add(component.getType());
+			var parameters = propertyMap.entrySet().stream()
+					.filter(e -> e.getKey().toString().equals(component.getName()))
+					.map(Map.Entry::getValue)
+					.findAny()
+					.orElse(null);
+			values.add(get(component.getType(), null, parameters, getValue));
 		}
-		return null;
+		var constructor = type.getDeclaredConstructor(types.toArray(new Class[0]));
+		constructor.setAccessible(true);
+		return constructor.newInstance(values.toArray());
+	}
+
+	private Object fromBuilder(Method builderFactory, Object value,
+							   Map<Attribute, Object> propertyMap, Function<Property, Object> getValue)
+			throws ReflectiveOperationException
+	{
+		var builder = builderFactory.invoke(null);
+		var build = Reflection.findMethod(builder.getClass(), "build").orElse(null);
+		if (build == null)
+			return null;
+		for (var entry : propertyMap.entrySet())
+		{
+			var attribute = entry.getKey();
+			var newValue = get(attribute.getRawType(), attribute.getValue(value), entry.getValue(), getValue);
+			Reflection.findMethod(builder.getClass(), attribute.toString(), attribute.getRawType())
+					.orElseThrow()
+					.invoke(builder, newValue);
+		}
+		return build.invoke(builder);
+	}
+
+	private Object fromAnemic(Class<?> type, Object value, Map<Attribute, Object> propertyMap, Function<Property, Object> getValue)
+			throws ReflectiveOperationException
+	{
+		if (value == null)
+			value = ObjectFactory.create(type);
+		for (var entry : propertyMap.entrySet())
+		{
+			var attribute = entry.getKey();
+			var currentValue = attribute.getValue(value);
+			var newValue = get(attribute.getRawType(), currentValue, entry.getValue(), getValue);
+			if (newValue != currentValue)
+				attribute.setValue(value, newValue);
+		}
+		return value;
+	}
+
+	private Object fromCanonicalConstructor(Class<?> type, Map<Attribute, Object> propertyMap, Function<Property, Object> getValue)
+			throws ReflectiveOperationException
+	{
+		var attributes = propertyMap.keySet();
+		var candidates = Arrays.stream(type.getDeclaredConstructors())
+				.filter(c ->
+				{
+					var params = c.getParameters();
+					return params.length == propertyMap.size()
+							&& Arrays.stream(params).allMatch(p -> attributes.stream().anyMatch(a -> a.matches(p)));
+				})
+				.toList();
+
+		if (candidates.size() != 1)
+			throw new ConversionException("Could not find canonical constructor for " + type.getName());
+
+		var constructor = candidates.get(0);
+		Object[] args = Arrays.stream(constructor.getParameters())
+				.map(p -> attributes.stream()
+						.filter(a -> a.matches(p))
+						.findFirst()
+						.map(e -> get(e.getRawType(), null, propertyMap.get(e), getValue))
+						.orElseThrow())
+				.toArray();
+		return constructor.newInstance(args);
 	}
 }
--- src/test/java/gate/lang/property/PropertyGraphTest.java
+++ src/test/java/gate/lang/property/PropertyGraphTest.java
@@ -4,6 +4,7 @@ import gate.entity.Auth;
 import gate.entity.Role;
 import gate.entity.User;
 import gate.error.BadRequestException;
+import gate.error.ConversionException;
 import gate.type.ID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -91,11 +92,195 @@ class PropertyGraphTest
 		Assertions.assertEquals(expected, result);
 	}
 
+	@Test
+	public void testIgnoreInvalidProperty() throws BadRequestException
+	{
+		var request = Map
+				.of("name", "Ana");
+
+		var properties = List.of("name", "doesNotExist");
+
+		var result = (User) PropertyGraph
+				.of(User.class, properties)
+				.get(null, prop -> request.get(prop.toString()));
+
+		Assertions.assertEquals("Ana", result.getName());
+	}
+
+	@Test
+	public void testUpdateExistingAnemicInstance() throws BadRequestException
+	{
+		var originalRole = new Role().setName("Old Role");
+		var original = new User()
+				.setName("Old")
+				.setRole(originalRole);
+
+		var request = Map
+				.of("name", "New");
+
+		var result = (User) PropertyGraph
+				.of(User.class, new ArrayList<>(request.keySet()))
+				.get(original, prop -> request.get(prop.toString()));
+
+		Assertions.assertSame(original, result);
+		Assertions.assertEquals("New", result.getName());
+		Assertions.assertSame(originalRole, result.getRole());
+	}
+
+	@Test
+	public void testBuilderType() throws BadRequestException
+	{
+		var request = Map
+				.of("name", "Builder Name",
+						"age", 33);
+
+		var previous = BuilderValue
+				.builder()
+				.name("old")
+				.age(1)
+				.build();
+
+		var result = (BuilderValue) PropertyGraph
+				.of(BuilderValue.class, new ArrayList<>(request.keySet()))
+				.get(previous, prop -> request.get(prop.toString()));
+
+		Assertions.assertEquals("Builder Name", result.getName());
+		Assertions.assertEquals(33, result.getAge());
+	}
+
+	@Test
+	public void testCanonicalConstructorAmbiguous() throws BadRequestException
+	{
+		var request = Map
+				.of("arg0", "A",
+						"arg1", 1);
+
+		var ex = Assertions.assertThrows(ConversionException.class, () -> {
+			PropertyGraph
+					.of(AmbiguousCanonical.class, new ArrayList<>(request.keySet()))
+					.get(null, prop -> request.get(prop.toString()));
+		});
+
+		Assertions.assertTrue(ex.getMessage().contains("Could not find canonical constructor"));
+	}
+
+	@Test
+	public void testCanonicalConstructorAmbiguousWithoutGetter() throws BadRequestException
+	{
+		var request = Map
+				.of("arg0", "A",
+						"arg1", 1);
+
+		var ex = Assertions.assertThrows(ConversionException.class, () -> {
+			PropertyGraph
+					.of(AmbiguousCanonicalWithoutGetter.class, new ArrayList<>(request.keySet()))
+					.get(null, prop -> request.get(prop.toString()));
+		});
+
+		Assertions.assertTrue(ex.getMessage().contains("Could not find canonical constructor"));
+	}
+
 	record Point(int x, int y)
 	{
 	}
 
 	record Line(Point start, Point end)
 	{
+	}
+
+	static class BuilderValue
+	{
+		private final String name;
+		private final Integer age;
+
+		private BuilderValue(Builder builder)
+		{
+			this.name = builder.name;
+			this.age = builder.age;
+		}
+
+		public static Builder builder()
+		{
+			return new Builder();
+		}
+
+		public String getName()
+		{
+			return name;
+		}
+
+		public Integer getAge()
+		{
+			return age;
+		}
+
+		static class Builder
+		{
+			private String name;
+			private Integer age;
+
+			public Builder name(String name)
+			{
+				this.name = name;
+				return this;
+			}
+
+			public Builder age(Integer age)
+			{
+				this.age = age;
+				return this;
+			}
+
+			public BuilderValue build()
+			{
+				return new BuilderValue(this);
+			}
+		}
+	}
+
+	static class AmbiguousCanonical
+	{
+		private final String arg0;
+		private final Integer arg1;
+
+		public AmbiguousCanonical(String arg0, Integer arg1)
+		{
+			this.arg0 = arg0;
+			this.arg1 = arg1;
+		}
+
+		public AmbiguousCanonical(Object arg0, Number arg1)
+		{
+			this.arg0 = String.valueOf(arg0);
+			this.arg1 = arg1 == null ? null : arg1.intValue();
+		}
+
+		public String getArg0()
+		{
+			return arg0;
+		}
+
+		public Integer getArg1()
+		{
+			return arg1;
+		}
+	}
+
+	static class AmbiguousCanonicalWithoutGetter
+	{
+		private final String arg0;
+		private final Integer arg1;
+
+		public AmbiguousCanonicalWithoutGetter(String arg0, Integer arg1)
+		{
+			this.arg0 = arg0;
+			this.arg1 = arg1;
+		}
+
+		public AmbiguousCanonicalWithoutGetter(Object arg0, Number arg1)
+		{
+			this.arg0 = String.valueOf(arg0);
+			this.arg1 = arg1 == null ? null : arg1.intValue();
+		}
 	}
 }
