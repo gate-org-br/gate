75faec511d891df56bf794039da28b0e2ea32070
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2026-03-15 01:28:35 -0300

    Improve unauthorized exception handling

--- src/main/java/gate/catcher/UnauthorizedExceptionCatcher.java
+++ src/main/java/gate/catcher/UnauthorizedExceptionCatcher.java
@@ -1,7 +1,11 @@
 package gate.catcher;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
 
 @ApplicationScoped
 public class UnauthorizedExceptionCatcher extends HttpExceptionCatcher
@@ -12,4 +16,20 @@ public class UnauthorizedExceptionCatcher extends HttpExceptionCatcher
 		super(HttpServletResponse.SC_UNAUTHORIZED);
 	}
 
+	@Override
+	public void catches(HttpServletRequest request,
+						HttpServletResponse response, Throwable exception)
+	{
+		if ("navigate".equalsIgnoreCase(request.getHeader("Sec-Fetch-Mode")))
+		{
+			try
+			{
+				response.sendRedirect("Gate");
+			} catch (IOException ex)
+			{
+				throw new UncheckedIOException(ex);
+			}
+		} else
+			super.catches(request, response, exception);
+	}
 }
