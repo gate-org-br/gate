128337b802e323a6067f328bce8bfae3800538f5
Davi Nunes da Silva <davinunesdasilva@gmail.com>
2026-03-18 20:27:32 -0300

    Improve UnauthorizedException handling

--- src/main/java/gate/catcher/HttpExceptionCatcher.java
+++ src/main/java/gate/catcher/HttpExceptionCatcher.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.core.HttpHeaders;
+
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.io.Writer;
@@ -15,9 +16,22 @@ public class HttpExceptionCatcher implements Catcher
 
 	@Override
 	public void catches(HttpServletRequest request,
-		HttpServletResponse response, Throwable exception)
+						HttpServletResponse response, Throwable exception)
 	{
+
 		HttpException httpException = (HttpException) exception;
+
+		if (httpException.getStatusCode() == HttpServletResponse.SC_UNAUTHORIZED
+			&& "navigate".equalsIgnoreCase(request.getHeader("Sec-Fetch-Mode")))
+		{
+			try
+			{
+				response.sendRedirect("Gate");
+			} catch (IOException ex)
+			{
+				throw new UncheckedIOException(ex);
+			}
+		}
 
 		response.setStatus(httpException.getStatusCode());
 		response.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain");
--- src/main/java/gate/error/UnauthorizedException.java
+++ src/main/java/gate/error/UnauthorizedException.java
@@ -1,7 +1,7 @@
 package gate.error;
 
 import gate.annotation.Catcher;
-import gate.catcher.UnauthorizedExceptionCatcher;
+import gate.catcher.HttpExceptionCatcher;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.Serial;
@@ -10,7 +10,7 @@ import java.io.Serial;
  * Signals that the client must authenticate itself to access the requested
  * resource.
  */
-@Catcher(UnauthorizedExceptionCatcher.class)
+@Catcher(HttpExceptionCatcher.class)
 public class UnauthorizedException extends HttpException
 {
 
